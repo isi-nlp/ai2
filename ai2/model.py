c6afc65ac365acf80953c48f5f46a91eea1db919
Chenghao <mouchenghao@gmail.com>
2019-08-30 10:44:23 -0700

    change val return shape

--- ai2/model.py
+++ ai2/model.py
@@ -26,6 +26,7 @@ class Classifier(pl.LightningModule):
         self.batch_size = batch_size
 
     def forward(self, x):
+
         output = self.model(x)[1]  # pooled context vectors
         return torch.sigmoid(self.linear(output))
 
@@ -40,13 +41,13 @@ class Classifier(pl.LightningModule):
         y_hat = self.forward(x)
 
         return {
-            'val_loss': (F.binary_cross_entropy(y_hat.reshape(-1), y.reshape(-1))).reshape((-1, 1)),
-            'val_acc': (((y_hat.reshape(-1) == y.reshape(-1)).sum()/y_hat.reshape(-1).size(0)).float()).reshape((-1, 1)),
+            'val_loss': (F.binary_cross_entropy(y_hat.reshape(-1), y.reshape(-1))).reshape((1, 1)),
+            'val_acc': (((y_hat.reshape(-1) == y.reshape(-1)).sum()/y_hat.reshape(-1).size(0)).float()).reshape((1, 1)),
             'val_f1': torch.tensor(f1_score(y.reshape(-1).cpu().detach().numpy().tolist(),
                                             (y_hat.reshape(-1) >= 0.5).long().cpu().detach().numpy().tolist()
-                                            ), requires_grad=False).to(x.device).reshape((-1, 1)),
-            'truth': y.reshape((-1, 1)),
-            'pred': y_hat.reshape((-1, 1)).long()}
+                                            ), requires_grad=False).to(x.device).reshape((1, 1)),
+            'truth': y.reshape((1, -1)),
+            'pred': y_hat.reshape((1, -1)).long()}
 
     def validation_end(self, outputs):
         avg_loss = torch.stack([x['val_loss'] for x in outputs], dim=-1).mean()
