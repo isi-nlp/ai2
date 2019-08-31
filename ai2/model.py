d8cf635b8d4601fdc312dbb2a58f3084c1d21fbe
Chenghao <mouchenghao@gmail.com>
2019-08-30 20:50:24 -0700

    improve utility code

--- ai2/model.py
+++ ai2/model.py
@@ -4,52 +4,64 @@ import numpy as np
 import pytorch_lightning as pl
 from torch.utils.data import Dataset, DataLoader
 from pytorch_transformers import *
-from ai2.utils import AI2Dataset, AI2Preprocessor
+from .utility import AI2DatasetHelper, AI2Dataset, collate_fn
 from torch.nn import functional as F
 from sklearn.metrics import f1_score, accuracy_score
+from typing import *
+from functools import partial
 
 
 class Classifier(pl.LightningModule):
 
-    def __init__(self, config, model_class, model_path, tokenizer_class, tokenizer_path, d_model, batch_size=64):
+    def __init__(self, config: Dict, model_class: callable, model_path: str, tokenizer_class: callable, tokenizer_path: str, d_model: int, batch_size: int = 64):
         super(Classifier, self).__init__()
+
+        assert 'classes' in config, "Wrong config for Classifier, classes not found"
+
         self.config = config
+
         self.model = model_class.from_pretrained(model_path, cache_dir='./.cache')
+        self.model.train()
         self.tokenizer = tokenizer_class.from_pretrained(tokenizer_path, cache_dir='./.cache', do_lower_case=False)
-        assert 'classes' in self.config, "Wrong config for Classifier, classes not found"
+
         self.linear = nn.Linear(d_model, 1)
         self.loss = nn.CrossEntropyLoss(reduction='sum')
 
-        self.preprocessor = AI2Preprocessor(self.config)
-        self.train_x, self.train_y, self.dev_x, self.dev_y = self.preprocessor.download()
+        self.helper = AI2DatasetHelper(self.config)
+        self.train_x, self.train_y, self.dev_x, self.dev_y = self.helper.download()
+
         self.d_model = d_model
         self.batch_size = batch_size
+        self.padding_index = self.tokenizer.convert_tokens_to_ids([self.tokenizer.pad_token])[0]
 
     def forward(self, x):
         """
-        x has shape [batch_size(B), num_choice(C), squence_length(S)]
+        Inputs:
+            x: [batch_size(B), num_choice(C), squence_length(S)]
+        Output: 
+            logits: [batch_size(B), num_choice(C)]
         """
         B, C, S = x.shape
-        output = self.model(x.reshape((-1, x.size(-1))))[1]  # pooled context vectors: [B*C, H]
-        output = output.reshape((B, C, -1))  # [B, C, H]
-        return torch.squeeze(self.linear(output), dim=-1)  # [B, C]
+        output = self.model(x.reshape((-1, x.size(-1))))[1]     # [B*C, H]
+        output = output.reshape((B, C, -1))                     # [B, C, H]
+        return torch.squeeze(self.linear(output), dim=-1)       # [B, C]
 
     def training_step(self, batch, batch_nb):
         x, y = batch['x'], batch['y']
-        y_hat = self.forward(x)  # [B, C]
-        assert y_hat.size(0) == y.size(0), "Batch size mismatch during training!"
+        y_hat = self.forward(x)                 # [B, C]
         return {'loss': self.loss(y_hat, y)}
 
     def validation_step(self, batch, batch_nb):
 
         x, y = batch['x'], batch['y']
         y_hat = self.forward(x)
-
         pred = y_hat.argmax(dim=-1)
 
+        # print(pred, y)
+
         return {
-            'batch_loss': (self.loss(y_hat, y)).reshape((1, 1)),
-            'batch_acc': (((pred == y).sum()/y_hat.size(0)).float()).reshape((1, 1)),
+            'batch_loss': self.loss(y_hat, y).reshape((1, 1)),
+            'batch_acc': ((pred == y).sum()/y_hat.size(0)).reshape((1, 1)),
             'batch_f1': torch.tensor(f1_score(y.reshape(-1).cpu().detach().numpy().tolist(),
                                               pred.cpu().detach().numpy().tolist()),
                                      requires_grad=False).to(x.device).reshape((1, 1)),
@@ -67,51 +79,34 @@ class Classifier(pl.LightningModule):
         }
 
     def configure_optimizers(self):
-        return torch.optim.AdamW(self.parameters(), lr=0.02)
-
-    def collate_fn(self, samples):
-        """
-        Pad x with the longest example in the batch
-        """
-        C = samples[0]['x'].size(0)
-
-        inputs = torch.zeros((len(samples), C, max(map(lambda x: x['x'].size(1), samples))), requires_grad=False)
-        inputs += self.tokenizer.convert_tokens_to_ids([self.tokenizer.pad_token])[0]
-        targets = torch.zeros((len(samples)))
-        for i, sample in enumerate(samples):
-            inputs[i, :, :sample['x'].size(1)] = sample['x']
-            targets[i] = sample['y']
-        return {'x': inputs.long(), 'y': targets.long()}
+        return torch.optim.AdamW(self.parameters(), lr=1e-3)
 
     @pl.data_loader
     def tng_dataloader(self):
         # REQUIRED
-        dataset = AI2Dataset(self.preprocessor.preprocess(self.train_x, self.train_y),
-                             self.tokenizer, self.tokenizer.convert_tokens_to_ids([self.tokenizer.pad_token])[0])
-        # dist_sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+        dataset = AI2Dataset(self.tokenizer, self.helper.preprocess(self.train_x, self.train_y))
         return DataLoader(dataset,
-                          #   sampler=dist_sampler,
-                          collate_fn=self.collate_fn,
+                          collate_fn=partial(collate_fn, padding_index=self.padding_index),
                           batch_size=self.batch_size)
 
     @pl.data_loader
     def val_dataloader(self):
         # OPTIONAL
-        dataset = AI2Dataset(self.preprocessor.preprocess(self.dev_x, self.dev_y),
-                             self.tokenizer, self.tokenizer.convert_tokens_to_ids([self.tokenizer.pad_token])[0])
-        # dist_sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+        dataset = AI2Dataset(self.tokenizer, self.helper.preprocess(self.dev_x, self.dev_y))
         return DataLoader(dataset,
-                          #   sampler=dist_sampler,
-                          collate_fn=self.collate_fn,
+                          collate_fn=partial(collate_fn, padding_index=self.padding_index),
                           batch_size=self.batch_size)
 
 
-# if __name__ == "__main__":
-#     from utils import TASKS
-#     from pytorch_lightning import Trainer
-#     from test_tube import Experiment
+if __name__ == "__main__":
 
-#     exp = Experiment('./output')
-#     model = Classifier(TASKS['anli'], BertModel, 'bert-base-uncased', BertTokenizer, 'bert-base-uncased', 768)
-#     trainer = Trainer(exp)
-#     trainer.fit(model)
+    from .utility import load_config
+    from pytorch_lightning import Trainer
+    from test_tube import Experiment
+
+    exp = Experiment('./output')
+    model = Classifier(load_config(
+        "/Users/chenghaomou/Code/Code-ProjectsPyCharm/ai2/ai2/tasks.yaml",
+    ), BertModel, 'bert-base-uncased', BertTokenizer, 'bert-base-uncased', 768, 2)
+    trainer = Trainer(exp)
+    trainer.fit(model)
---
+++ ai2/utility.py
@@ -0,0 +1,199 @@
+# encoding: utf-8
+# Description: Utility functions for data preprocessing and loading.
+# Author: Chenghao Mou
+# Last updated: Aug 30, 2019
+
+import numpy as np
+import torch
+import os
+import requests
+import yaml
+import json
+from dataclasses import dataclass
+from typing import *
+from torch.utils.data import Dataset
+from sklearn.metrics import accuracy_score, f1_score
+from pathlib import Path
+from io import BytesIO
+from zipfile import ZipFile
+from loguru import logger
+from pytorch_transformers.tokenization_utils import PreTrainedTokenizer
+
+
+@dataclass
+class Pair:
+
+    premise: str
+    hypothesis: str
+
+
+@dataclass
+class Example:
+
+    pairs: List[Pair]
+    label: int   # Zero-indexed true label
+
+
+@dataclass
+class AI2DatasetHelper:
+
+    config: Dict
+    filename: str = None
+    cache_dir: str = './.cache'
+
+    examples: List = None
+
+    def __post_init__(self):
+
+        assert 'url' in self.config, 'Download url is missing'
+
+        self.filename = self.config['url'].rsplit('/', 1)[-1]
+
+        self.cache_dir = Path(self.cache_dir)
+
+    def download(self) -> Tuple[List[Dict], List[str], List[Dict], List[str]]:
+        """Download both train and dev datasets."""
+        if not os.path.exists(self.cache_dir):
+            os.mkdir(self.cache_dir)
+        if not os.path.exists(self.cache_dir / self.filename):
+            request = requests.get(self.config['url'])
+            with open(self.cache_dir / self.filename, 'wb') as output:
+                output.write(request.content)
+
+        with open(self.cache_dir / self.filename, "rb") as input_file:
+            zipped_file = ZipFile(BytesIO(input_file.read()))
+
+        # Check expected fields with actual ones.
+        expected = set(self.config['format'].values())
+        found = set(f for f in zipped_file.namelist() if f.endswith(('jsonl', 'lst')) and not f.startswith(('__', '.')))
+
+        assert expected == found, f'Dismatched files in downloaded file, looking for {exp} in {found}'
+
+        train_x = list(map(json.loads, zipped_file.open(self.config['format'].get('train_x', None)).readlines()))
+        train_y = zipped_file.open(self.config['format'].get('train_y', None)).readlines()
+        dev_x = list(map(json.loads, zipped_file.open(self.config['format'].get('dev_x', None)).readlines()))
+        dev_y = zipped_file.open(self.config['format'].get('dev_y', None)).readlines()
+
+        logger.info(f"""Training examples: {len(train_x)}, Dev examples: {len(dev_x)}""")
+
+        return train_x, train_y, dev_x, dev_y
+
+    def preprocess(self, input_x: List[Dict], input_y: List[str]) -> List[Example]:
+
+        examples = []
+        premise_keys, choice_keys = self.parse_formula()
+
+        for raw_example, raw_label in zip(input_x, input_y):
+
+            choices = raw_example[choice_keys[0]] if len(choice_keys) == 1 else [raw_example[choice_key] for choice_key in choice_keys]
+            premise = ' '.join(raw_example.get(premise_key, "") for premise_key in premise_keys)
+
+            examples.append(
+                Example(
+                    [
+                        Pair(premise, choice) for choice in choices
+                    ],
+                    int(raw_label) - self.config['start']
+                )
+            )
+
+        return examples
+
+    def parse_formula(self):
+
+        assert 'formula' in self.config, 'Formula is missing'
+
+        def strip(l: List[str]) -> List[str]:
+            return [x.strip() for x in l]
+
+        premise_keys, choice_keys = self.config['formula'].split('=>')
+        premise_keys = strip(premise_keys.split('+'))
+        choice_keys = strip(choice_keys.split('|'))
+
+        return premise_keys, choice_keys
+
+
+@dataclass
+class AI2Dataset(Dataset):
+
+    tokenizer: PreTrainedTokenizer
+    examples: List[Example]
+    padding_index: int = None
+
+    def __post_init__(self):
+        self.padding_index = self.tokenizer.convert_tokens_to_ids([self.tokenizer.pad_token])[0]
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, index):
+
+        example = self.examples[index]
+
+        input_ids = []
+
+        for pair in example.pairs:
+
+            # [CLS] TOK TOK ... [SEP] TOK TOK ... [SEP]
+
+            tokens = [self.tokenizer.cls_token] + self.tokenizer.tokenize(pair.premise) +  \
+                [self.tokenizer.sep_token] + self.tokenizer.tokenize(pair.hypothesis) + [self.tokenizer.sep_token]
+
+            input_ids.append(np.asarray(self.tokenizer.convert_tokens_to_ids(tokens)))
+
+        input_tensor = pad_list(input_ids, self.tokenizer.convert_tokens_to_ids([self.tokenizer.pad_token])[0])
+
+        return {
+            'x': input_tensor.long(),
+            'y': torch.tensor(example.label).reshape((1,)).long()
+        }
+
+
+def pad_list(l: Union[List[np.ndarray], List[torch.Tensor]], padding_index: int) -> torch.LongTensor:
+
+    l = list([np.asarray(x) if isinstance(x, list) else x for x in l])
+    assert all(x.shape[:-1] == l[0].shape[:-1] for x in l), 'Mismatch dimensions for list elements!'
+    *dimensions, _ = l[0].shape
+    placeholder = torch.zeros((len(l), *dimensions, max(map(lambda x: x.shape[-1], l))), requires_grad=False).long() + padding_index
+
+    for i, x in enumerate(l):
+        placeholder[i, ..., :x.shape[-1]] = torch.tensor(x).long() if isinstance(x, np.ndarray) else x
+
+    return placeholder
+
+
+def load_config(path: Path, name: str = 'anli') -> Dict:
+
+    with open(path) as f:
+        return yaml.load(f, Loader=yaml.FullLoader)[name]
+
+
+def collate_fn(exmples, padding_index: int):
+
+    return {
+        'x': pad_list([x['x'] for x in exmples], padding_index),
+        'y': pad_list([x['y'] for x in exmples], padding_index).reshape(-1)
+    }
+
+
+if __name__ == "__main__":
+
+    print(pad_list([[1, 2], [2, 3, 4]], 0))
+    print(pad_list([[[1], [2]], [[2, 1], [3, 4]]], 0))
+
+    config = load_config('/Users/chenghaomou/Code/Code-ProjectsPyCharm/ai2/ai2/tasks.yaml')
+    helper = AI2DatasetHelper(config)
+    train_x, train_y, dev_x, dev_y = helper.download()
+    train_examples = helper.preprocess(train_x, train_y)
+
+    # print(train_examples[0])
+
+    from pytorch_transformers import *
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-cased', cache_dir='./.cache', do_lower_case=False)
+    dataset = AI2Dataset(tokenizer, train_examples)
+
+    # print(dataset[0])
+
+    batch_x, batch_y = collate_fn([dataset[0], dataset[1], dataset[4]], tokenizer.convert_tokens_to_ids([tokenizer.pad_token])[0])
+    print(batch_x.shape, batch_y.shape)
