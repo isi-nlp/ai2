e11d0c0d0d4dd6ee35ca07058376671ca6dc8246
Chenghao <mouchenghao@gmail.com>
2019-09-01 21:10:38 -0700

    update readme

--- README.md
+++ README.md
@@ -1,10 +1,48 @@
 # ai2
 
-Framework for testing models with AI2 leaderboards
+Framework for testing models with AI2 leaderboards, integrated with all kinds of fancy features supported by pytorch-lightning, such as multi GPU training/tensorboard support, and most importantly, readability!
 
-# Quick start
+## 1. Quick start
 
 ```bash
 python run_darpa.py --help
-python run_darpa.py --task anli --model_type bert --tokenizer_type bert --model_weight bert-base-cased --tokenizer_weight bert-base-cased --config_type bert --config_weight bert-base-cased --batch_size 2
+
+python run_darpa.py                         \
+    --task anli                             \
+    --model_type bert                       \
+    --model_weight bert-base-cased          \
+    --tokenizer_type bert                   \
+    --tokenizer_weight bert-base-cased      \
+    --model_config_type bert                \
+    --model_config_weight bert-base-cased   \
+    --train_config ai2/base-task.yaml
 ```
+
+**If you are using HPC nodes, you should run this script first on your login node to cache the task dataset first.**
+
+### 1.1 Task
+
+One of the four tasks from ai2 leaderboards:
+
+1. `anli`: _Abductive Natural Language Inference (aNLI)_
+2. `hellaswag`: _HellaSwag: Can a Machine Really Finish Your Sentence?_
+3. `pyhsicaliqa`: _Physical IQa: Physical Interaction QA_
+4. `socialiqa`: _Social IQA: Social Interaction QA_
+
+### 1.2 Model/Tokenizer/Weight type
+
+Seven models supported by huggingface:
+
+1. `bert`: BERT (from Google) released with the paper BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding by Jacob Devlin, Ming-Wei Chang, Kenton Lee and Kristina Toutanova.
+2. `gpt`: GPT (from OpenAI) released with the paper Improving Language Understanding by Generative Pre-Training by Alec Radford, Karthik Narasimhan, Tim Salimans and Ilya Sutskever.
+3. `gpt2`: GPT-2 (from OpenAI) released with the paper Language Models are Unsupervised Multitask Learners by Alec Radford*, Jeffrey Wu*, Rewon Child, David Luan, Dario Amodei** and Ilya Sutskever**.
+4. `transformerxl`: Transformer-XL (from Google/CMU) released with the paper Transformer-XL: Attentive Language Models Beyond a Fixed-Length Context by Zihang Dai*, Zhilin Yang*, Yiming Yang, Jaime Carbonell, Quoc V. Le, Ruslan Salakhutdinov.
+5. `xlnet`: XLNet (from Google/CMU) released with the paper ​XLNet: Generalized Autoregressive Pretraining for Language Understanding by Zhilin Yang*, Zihang Dai*, Yiming Yang, Jaime Carbonell, Ruslan Salakhutdinov, Quoc V. Le.
+6. `xlm`: XLM (from Facebook) released together with the paper Cross-lingual Language Model Pretraining by Guillaume Lample and Alexis Conneau.
+7. `roberta`: RoBERTa (from Facebook), released together with the paper a Robustly Optimized BERT Pretraining Approach by Yinhan Liu, Myle Ott, Naman Goyal, Jingfei Du, Mandar Joshi, Danqi Chen, Omer Levy, Mike Lewis, Luke Zettlemoyer, Veselin Stoyanov.
+
+**Model type** means one of the seven models while **model weight** means different pretrained model weight like `bert-base-cased`. The tokenizer/tokenizer_weight normally take the same type/weight. **Model config** is the huggingface's config class/weight which tells some superparameters about a model and the training settings, like batch size or learning rate. Please refer to huggingface's [repo](https://github.com/huggingface/pytorch-transformers) for more information.
+
+### 1.3 Train config
+
+You can specify your own training config file with YAML for batch size, learning rate, max sequence length, or max epochs.
--- run_darpa.py
+++ run_darpa.py
@@ -46,13 +46,16 @@ if __name__ == "__main__":
     parser.add_argument('--train_config', help='Training config file', required=True)
     parser.add_argument('--model_type', choices=MODELS, help='Model type', required=True)
     parser.add_argument('--tokenizer_type', choices=TOKENIZERS, help='Tokenizer type', required=True)
-    parser.add_argument('--model_config_type', choices=CONFIGS, help='Model configuration type', required=True)
+    parser.add_argument('--model_config_type', choices=CONFIGS, help='Model configuration type', required=False)
     parser.add_argument('--model_weight', help='Model weight from huggingface', required=True)
     parser.add_argument('--tokenizer_weight', help='Pretrained tokenizer from huggingface', required=True)
-    parser.add_argument('--model_config_weight', help='Predefined configuration', required=True)
-    parser.add_argument('--batch_size', type=int, help='Batch size')
+    parser.add_argument('--model_config_weight', help='Predefined configuration', required=False)
 
     args = parser.parse_args()
+
+    if args.model_config_weight is None or args.model_config_type is None:
+        args.model_config_weight = args.model_weight
+        args.model_config_type = args.model_type
 
     TASK = load_config("ai2/tasks.yaml", args.task)
 
@@ -65,33 +68,14 @@ if __name__ == "__main__":
                        tokenizer_path=args.tokenizer_weight,
                        model_config_class=CONFIGS[args.model_config_type],
                        model_config_path=args.model_config_weight)
-    trainer = Trainer(exp,
-                      early_stop_callback=EarlyStopping(monitor='val_f1', patience=10, mode='max'),
-                      checkpoint_callback=ModelCheckpoint(filepath=f'./{args.task}-{args.model_weight}-models', monitor='val_f1', save_best_only=True),
-                      gradient_clip=1.0,
-                      cluster=None,
-                      process_position=0,
-                      current_gpu_name=0,
-                      nb_gpu_nodes=1,
+    trainer = Trainer(exp, early_stop_callback=EarlyStopping(monitor='val_f1', patience=10, mode='max'),
+                      checkpoint_callback=ModelCheckpoint(
+                          filepath=f'./{args.task}-{args.model_weight}-models', monitor='val_f1', save_best_only=True),
+                      gradient_clip=1.0, cluster=None, process_position=0, current_gpu_name=0, nb_gpu_nodes=1,
                       gpus=[i for i in range(torch.cuda.device_count())],
-                      show_progress_bar=True,
-                      overfit_pct=0.0,
-                      track_grad_norm=-1,
-                      check_val_every_n_epoch=1,
-                      fast_dev_run=True,
-                      accumulate_grad_batches=1,
-                      max_nb_epochs=load_config(args.train_config)['max_epochs'],
-                      min_nb_epochs=1,
-                      train_percent_check=1.0,
-                      val_percent_check=1.0,
-                      test_percent_check=1.0,
-                      val_check_interval=0.1,
-                      log_save_interval=10,
-                      add_log_row_interval=10,
-                      distributed_backend='dp',
-                      use_amp=False,
-                      print_nan_grads=False,
-                      print_weights_summary=False,
-                      amp_level='O2',
-                      nb_sanity_val_steps=5)
+                      show_progress_bar=True, overfit_pct=0.0, track_grad_norm=-1, check_val_every_n_epoch=1, fast_dev_run=True,
+                      accumulate_grad_batches=1, max_nb_epochs=load_config(args.train_config)['max_epochs'],
+                      min_nb_epochs=1, train_percent_check=1.0, val_percent_check=1.0, test_percent_check=1.0, val_check_interval=0.1,
+                      log_save_interval=10, add_log_row_interval=10, distributed_backend='dp', use_amp=False, print_nan_grads=False,
+                      print_weights_summary=False, amp_level='O2', nb_sanity_val_steps=5)
     trainer.fit(model)
