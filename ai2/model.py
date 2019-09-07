ee932a1fabb0ee3e33f8fd5713fa455df7a776c4
Chenghao <mouchenghao@gmail.com>
2019-09-07 11:56:47 -0700

    update logic

--- ai2/model.py
+++ ai2/model.py
@@ -1,177 +1,262 @@
+# encoding: utf-8
+# Created by chenghaomou at 9/7/19
+# Contact: mouchenghao at gmail dot com
+# Description: Model template
+
+"""
+Example template for defining a system
+"""
+from collections import OrderedDict
+
+import pytorch_lightning as pl
 import torch
 import torch.nn as nn
-import numpy as np
-import pytorch_lightning as pl
-import inspect
-from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+from pytorch_lightning.root_module.root_module import LightningModule
 from pytorch_transformers import *
-from ai2.utility import AI2DatasetHelper, AI2Dataset, collate_fn
-from torch.nn import functional as F
-from sklearn.metrics import f1_score, accuracy_score
-from typing import *
-from functools import partial
-from loguru import logger
+from sklearn.metrics import accuracy_score
+from test_tube import HyperOptArgumentParser
+from torch import optim
+from torch.utils.data import DataLoader
+
+from ai2.interface import PyTorchTransformer, PyTorchTransformerTokenizer
+from ai2.utils import AI2Dataset, Formulator, Preprocessor
+
+MODELS = {
+    'bert': BertModel,
+    'xlm': XLMModel,
+    'xlnet': XLNetModel,
+    'roberta': RobertaModel,
+    'gpt': OpenAIGPTModel,
+    'gpt2': GPT2Model
+}
+
+TOKENIZERS = {
+    'bert': BertTokenizer,
+    'xlm': XLMTokenizer,
+    'xlnet': XLNetTokenizer,
+    'roberta': RobertaTokenizer,
+    'gpt': OpenAIGPTTokenizer,
+    'gpt2': GPT2Tokenizer
+
+}
 
 
-def forward(model, input_ids, token_type_ids, attention_mask):
-    """Ignore some of the inputs according to model's function signature."""
-    spec = inspect.getargspec(model.forward)
+class Classifier(LightningModule):
+    """
+    Sample model to show how to define a template
+    """
 
-    args = {'input_ids': input_ids}
-    if 'token_type_ids' in spec:
-        args['token_type_ids'] = token_type_ids
-    if 'attention_mask' in spec:
-        args['attention_mask'] = attention_mask
-
-    return model.forward(**args)
-
-
-class Classifier(pl.LightningModule):
-
-    def __init__(self,
-                 task_config: Dict,
-                 train_config: Dict,
-                 model_class: callable,
-                 model_path: str,
-                 tokenizer_class: callable,
-                 tokenizer_path: str,
-                 model_config_class: callable,
-                 model_config_path: str):
+    def __init__(self, hparams):
+        """
+        Pass in parsed HyperOptArgumentParser to the model
+        :param hparams:
+        """
+        # init superclass
         super(Classifier, self).__init__()
+        self.hparams = hparams
+        self.formulator = Formulator({
+            'premise': self.hparams.task_premise.split('+'),
+            'hypotheses': self.hparams.task_hypotheses.split('|')
+        })
+        self.tokenizer = PyTorchTransformerTokenizer.load(TOKENIZERS[self.hparams.tokenizer_type],
+                                                          self.hparams.tokenizer_weight)
+        self.preprocessor = Preprocessor(self.tokenizer, self.formulator, lambda y: y - self.hparams.task_offset)
 
-        assert 'classes' in task_config, "Wrong config for Classifier, classes not found"
+        # build model
+        self.__build_model()
 
-        self.task_config = task_config
-        self.train_config = train_config
-
-        self.model = model_class.from_pretrained(model_path, cache_dir='./.cache')
-        self.model_config = model_config_class.from_pretrained(model_config_path, cache_dir='./.cache')
-        self.model.train()
-
-        dropout = self.model_config.__dict__.get(
-            'hidden_dropout_prob',
-            self.model_config.__dict__.get(
-                'resid_pdrop',
-                self.model_config.__dict__.get(
-                    'dropout',
-                    0.1
-                )
-            )
-        )
-
-        hidden_size = self.model_config.__dict__.get(
-            'hidden_size',
-            self.model_config.__dict__.get(
-                'n_embd',
-                self.model_config.__dict__.get(
-                    'd_model',
-                    self.model_config.__dict__.get(
-                        'emb_dim',
-                        None
-                    )
-                )
-            )
-        )
-
-        initializer_range = self.model_config.__dict__.get(
-            'init_range',
-            self.model_config.__dict__.get(
-                'initializer_range',
-                0.02
-
-            )
-        )
-
-        logger.debug(f'Dropout: {dropout}')
-        logger.info(f'Hidden size: {hidden_size}')
-        logger.info(f'Initializer range: {initializer_range}')
-
-        self.dropout = nn.Dropout(dropout)
-        self.linear = nn.Linear(hidden_size, 1)
-        self.linear.weight.data.normal_(mean=0.0, std=initializer_range)
+    # ---------------------
+    # MODEL SETUP
+    # ---------------------
+    def __build_model(self):
+        """
+        Layout model
+        :return:
+        """
+        self.base = PyTorchTransformer.load(MODELS[self.hparams.model_type], self.hparams.model_weight)
+        self.dropout = nn.Dropout(self.hparams.train_dropout)
+        self.linear = nn.Linear(self.base.dimension, 1)
+        self.linear.weight.data.normal_(mean=0.0, std=self.hparams.train_initializer_range)
         self.linear.bias.data.zero_()
+        # ---------------------
 
-        self.tokenizer = tokenizer_class.from_pretrained(tokenizer_path, cache_dir='./.cache', do_lower_case=False)
-
-        self.loss = nn.CrossEntropyLoss(reduction='sum')
-        self.helper = AI2DatasetHelper(self.task_config)
-        self.train_x, self.train_y, self.dev_x, self.dev_y = self.helper.download()
-        self.batch_size = self.train_config['batch_size']
-
-        self.index = 1 if 'bert' in model_class.__class__.__name__ else 0
-
-    def forward(self, x, token_type_ids, attention_mask):
+    # TRAINING
+    # ---------------------
+    def forward(self, input_ids, token_type_ids, attention_mask):
         """
-        Inputs:
-            x: [batch_size(B), num_choice(C), squence_length(S)]
-        Output:
-            logits: [batch_size(B), num_choice(C)]
+        No special modification required for lightning, define as you normally would
+        :param x:
+        :return:
         """
-        B, C, S = x.shape
 
-        pooled_output = forward(self.model,
-                                input_ids=x.reshape((B*C, S)),
-                                token_type_ids=token_type_ids.reshape((B*C, S)),
-                                attention_mask=attention_mask.reshape((B*C, S)))[self.index]    # [B*C, H]
-        if len(pooled_output.shape) == 3:
-            pooled_output = pooled_output.mean(dim=1)  # [B*C, S, H] => [B*C, H]
-        pooled_output = self.dropout(pooled_output)
-        logits = self.linear(pooled_output)
-        reshaped_logits = logits.view(-1, C)
+        B, C, S = input_ids.shape
 
-        return reshaped_logits
+        output = self.base.forward(input_ids=input_ids.reshape(B * C, S),
+                                   token_type_ids=token_type_ids.reshape(B * C, S),
+                                   attention_mask=attention_mask.reshape(B * C, S))[0]
 
-    def training_step(self, batch, batch_nb):
-        x, y, token_type_ids, attention_mask = batch['x'], batch['y'], batch['token_type_ids'], batch['attention_mask']
-        y_hat = self.forward(x, token_type_ids, attention_mask)                 # [B, C]
-        return {'loss': self.loss(y_hat, y)}
+        if len(output.shape) == 3:
+            output = output.mean(dim=1).reshape((B * C, -1))
 
-    def validation_step(self, batch, batch_nb):
+        output = self.dropout(output)
+        logits = self.linear(output).reshape((-1, C))
 
-        x, y, token_type_ids, attention_mask = batch['x'], batch['y'], batch['token_type_ids'], batch['attention_mask']
-        y_hat = self.forward(x, token_type_ids, attention_mask)
-        pred = y_hat.argmax(dim=-1)
-        prob = F.softmax(y_hat, dim=-1)
+        return logits
 
-        return {
-            'batch_loss': self.loss(y_hat, y).reshape((1, 1)),
-            'batch_acc': ((pred == y).sum()/y_hat.size(0)).reshape((1, 1)),
-            'batch_f1': torch.tensor(f1_score(y.reshape(-1).cpu().detach().numpy().tolist(),
-                                              pred.cpu().detach().numpy().tolist(), average='micro'),
-                                     requires_grad=False).to(x.device).reshape((1, 1)),
+    def loss(self, labels, logits):
+        l = F.cross_entropy(logits, labels, reduction='sum', ignore_index=self.tokenizer.pad_index)
+        return l
+
+    def training_step(self, data_batch, batch_i):
+        """
+        Lightning calls this inside the training loop
+        :param data_batch:
+        :return:
+        """
+        # forward pass
+        input_ids, y, token_type_ids, attention_mask = data_batch['input_ids'], data_batch['y'], data_batch[
+            'token_type_ids'], data_batch['attention_mask']
+        y_hat = self.forward(input_ids, token_type_ids, attention_mask)
+        loss_val = self.loss(y, y_hat)
+
+        if self.trainer.use_dp:
+            loss_val = loss_val.unsqueeze(0)
+
+        output = OrderedDict({
+            'loss': loss_val
+        })
+
+        return output
+
+    def validation_step(self, data_batch, batch_i):
+        """
+        Lightning calls this inside the validation loop
+        :param data_batch:
+        :return:
+        """
+        input_ids, y, token_type_ids, attention_mask = data_batch['input_ids'], data_batch['y'], data_batch[
+            'token_type_ids'], data_batch['attention_mask']
+        y_hat = self.forward(input_ids, token_type_ids, attention_mask)
+        loss_val = self.loss(y, y_hat)
+
+        # acc
+        labels_hat = torch.argmax(y_hat, dim=-1)
+        val_acc = torch.sum(y == labels_hat).item() / (len(y) * 1.0)
+        val_acc = torch.tensor(val_acc)
+
+        if self.on_gpu:
+            val_acc = val_acc.cuda(loss_val.device)
+
+        # in DP mode (default) make sure if result is scalar, there's another dim in the beginning
+        if self.trainer.use_dp:
+            loss_val = loss_val.unsqueeze(0)
+            val_acc = val_acc.unsqueeze(0)
+
+        output = OrderedDict({
+            'batch_loss': loss_val,
+            'batch_acc': val_acc,
             'truth': y,
-            'pred': pred,
-            'prob': prob}
+            'pred': labels_hat,
+            'prob': F.softmax(y_hat, dim=-1)
+        })
+
+        # can also return just a scalar instead of a dict (return loss_val)
+        return output
 
     def validation_end(self, outputs):
+        """
+        Called at the end of validation to aggregate outputs
+        :param outputs: list of individual outputs of each validation step
+        :return:
+        """
+        # if returned a scalar from validation_step, outputs is a list of tensor scalars
+        # we return just the average in this case (if we want)
+        # return torch.stack(outputs).mean()
 
-        truth = torch.cat([x['truth'] for x in outputs], dim=0).reshape(-1)
-        pred = torch.cat([x['pred'] for x in outputs], dim=0).reshape(-1)
+        truth = torch.cat([output['truth'] for output in outputs], dim=0).reshape(-1).cpu().detach().numpy().tolist()
+        pred = torch.cat([output['pred'] for output in outputs], dim=0).reshape(-1).cpu().detach().numpy().tolist()
 
-        return {
-            'val_acc': accuracy_score(truth.cpu().detach().numpy().tolist(), pred.cpu().detach().numpy().tolist()),
-            'val_f1': f1_score(truth.cpu().detach().numpy().tolist(), pred.cpu().detach().numpy().tolist(), average='micro')
-        }
+        prob = torch.cat([output['prob'] for output in outputs], dim=0).cpu().detach().numpy().tolist()
 
+        tqdm_dic = {'val_acc': accuracy_score(truth, pred)}
+        return tqdm_dic
+
+    # ---------------------
+    # TRAINING SETUP
+    # ---------------------
     def configure_optimizers(self):
-        return torch.optim.AdamW(self.parameters(), lr=float(self.train_config['lr']))
+        """
+        return whatever optimizers we want here
+        :return: list of optimizers
+        """
+        optimizer = optim.Adam(self.parameters(), lr=self.hparams.train_learning_rate)
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
+        return [optimizer], [scheduler]
 
     @pl.data_loader
     def tng_dataloader(self):
-        # REQUIRED
-        dataset = AI2Dataset(tokenizer=self.tokenizer,
-                             examples=self.helper.preprocess(self.train_x, self.train_y),
-                             max_sequence_length=self.train_config['max_sequence_length'])
-        return DataLoader(dataset,
-                          collate_fn=partial(collate_fn, padding_index=self.tokenizer.convert_tokens_to_ids([dataset.pad_token])[0]),
-                          batch_size=self.batch_size)
+
+        return DataLoader(
+            AI2Dataset.load(self.hparams.dataset_cache_dir,
+                            mapping={self.hparams.dataset_train_x: 'input', self.hparams.dataset_train_y: 'label'},
+                            transform=self.preprocessor),
+            batch_size=self.hparams.train_batch_size,
+            collate_fn=self.preprocessor.collate,
+            shuffle=True
+        )
 
     @pl.data_loader
     def val_dataloader(self):
-        # OPTIONAL
-        dataset = AI2Dataset(tokenizer=self.tokenizer,
-                             examples=self.helper.preprocess(self.dev_x, self.dev_y),
-                             max_sequence_length=self.train_config['max_sequence_length'])
-        return DataLoader(dataset,
-                          collate_fn=partial(collate_fn, padding_index=self.tokenizer.convert_tokens_to_ids([dataset.pad_token])[0]),
-                          batch_size=self.batch_size)
+        return DataLoader(
+            AI2Dataset.load(self.hparams.dataset_cache_dir,
+                            mapping={self.hparams.dataset_dev_x: 'input', self.hparams.dataset_dev_y: 'label'},
+                            transform=self.preprocessor),
+            batch_size=self.hparams.train_batch_size,
+            collate_fn=self.preprocessor.collate,
+            shuffle=False
+        )
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):  # pragma: no cover
+        """
+        Parameters you define here will be available to your model through self.hparams
+        :param parent_parser:
+        :return:
+        """
+        parser = HyperOptArgumentParser(strategy=parent_parser.strategy, parents=[parent_parser], add_help=False)
+
+        # param overwrites
+        parser.set_defaults(gradient_clip=1.0)
+
+        training_group = parser.add_argument_group(title='Training options')
+        base_model_group = parser.add_argument_group(title='Model options')
+        base_tokenizer_group = parser.add_argument_group(title='Tokenizer options')
+        dataset_group = parser.add_argument_group(title='Dataset options')
+        task_group = parser.add_argument_group(title='Task options')
+
+        # Add arguments to those groups
+
+        training_group.add_argument('--train_learning_rate', type=float, default=2e-5)
+        training_group.add_argument('--train_batch_size', type=int, default=8)
+        training_group.add_argument('--train_dropout', type=float, default=0.1)
+        training_group.add_argument('--train_initializer_range', type=float, default=0.02)
+
+        base_model_group.add_argument('--model_type', type=str, default='bert', required=True)
+        base_model_group.add_argument('--model_weight', type=str, default='bert-base-cased', required=True)
+
+        base_tokenizer_group.add_argument('--tokenizer_type', type=str, default='bert', required=True)
+        base_tokenizer_group.add_argument('--tokenizer_weight', type=str, default='bert-base-cased', required=True)
+
+        dataset_group.add_argument('--dataset_cache_dir', type=str, required=True)
+        dataset_group.add_argument('--dataset_train_x', type=str, required=True)
+        dataset_group.add_argument('--dataset_train_y', type=str, required=True)
+        dataset_group.add_argument('--dataset_dev_x', type=str, required=True)
+        dataset_group.add_argument('--dataset_dev_y', type=str, required=True)
+
+        task_group.add_argument('--task_name', choices=['anli', 'hellaswag', 'physicaliqa', 'socialiqa'], required=True)
+        task_group.add_argument('--task_premise', type=str, required=True)
+        task_group.add_argument('--task_hypotheses', type=str, required=True)
+        task_group.add_argument('--task_offset', type=int, required=True)
+
+        return parser
