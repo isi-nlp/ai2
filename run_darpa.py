84973ccf8ea3e5090d40baa980056ebbf08a19e0
Chenghao <mouchenghao@gmail.com>
2019-09-09 12:01:55 -0700

    fix typo

--- run_darpa.py
+++ run_darpa.py
@@ -72,21 +72,16 @@ if __name__ == "__main__":
                        model_config_class=CONFIGS[args.model_config_type],
                        model_config_path=args.model_config_weight)
 
-    trainer = Trainer(exp,
-                      early_stop_callback=EarlyStopping(monitor='val_f1', patience=10, mode='max'),
+    trainer = Trainer(exp, early_stop_callback=EarlyStopping(monitor='val_f1', patience=10, mode='max'),
                       checkpoint_callback=ModelCheckpoint(
-                          filepath=f'./{args.task}-{args.model_weight}-{args.train_config.split('/')[-1]}-models', monitor='val_f1', save_best_only=False,
-                          mode='max'),
+                          filepath=f"./{args.task}-{args.model_weight}-{args.train_config.split('/')[-1]}-models", monitor='val_f1',
+                          save_best_only=False, mode='max'),
                       gradient_clip=1.0, cluster=None, process_position=0, current_gpu_name=0, nb_gpu_nodes=1,
                       gpus=[i for i in range(torch.cuda.device_count())],
-                      show_progress_bar=True, overfit_pct=0.0, track_grad_norm=-1, check_val_every_n_epoch=1,
-                      fast_dev_run=args.debug,
+                      show_progress_bar=True, overfit_pct=0.0, track_grad_norm=-1, check_val_every_n_epoch=1, fast_dev_run=args.debug,
                       accumulate_grad_batches=1, max_nb_epochs=load_config(args.train_config)['max_epochs'],
-                      min_nb_epochs=0, train_percent_check=0.001 if args.debug else 1.0,
-                      val_percent_check=0.001 if args.debug else 1.0,
-                      test_percent_check=1.0, val_check_interval=0.1, log_save_interval=50, add_log_row_interval=50,
-                      distributed_backend='dp',
-                      use_amp=False, print_nan_grads=False, print_weights_summary=False, amp_level='O2',
-                      nb_sanity_val_steps=2 if args.debug else 5)
+                      min_nb_epochs=0, train_percent_check=0.001 if args.debug else 1.0, val_percent_check=0.001 if args.debug else 1.0,
+                      test_percent_check=1.0, val_check_interval=0.1, log_save_interval=50, add_log_row_interval=50, distributed_backend='dp',
+                      use_amp=False, print_nan_grads=False, print_weights_summary=False, amp_level='O2', nb_sanity_val_steps=2 if args.debug else 5)
 
     trainer.fit(model)
