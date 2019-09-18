298adec36529d26c5d626c24382dfaf93e07a639
Chenghao <mouchenghao@gmail.com>
2019-09-18 09:57:39 -0700

    update training script

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
-PYTHON=/auto/nlg-05/chengham/anaconda3/envs/py37/bin/python 
+PYTHON=/auto/nlg-05/chengham/anaconda3/envs/py37/bin/python
 TRAIN=train.py
 
-$PYTHON $TRAIN --model_type $1 --model_weight $2 \
-    --task_config_file tasks.yaml \
-    --running_config_file hyparams.yaml \
-    --task_name $3 \
-    --task_cache_dir ./cache \
-    --output_dir output/$3-$1-$2-pred \
-    --log_save_interval 25 --add_log_row_interval 25
+$PYTHON -W ignore $TRAIN --model_type $1 --model_weight $2 \
+  --task_config_file tasks.yaml \
+  --running_config_file hyparams.yaml \
+  --task_name $3 \
+  --task_cache_dir ./cache \
+  --output_dir output/$3-$1-$2-pred \
+  --log_save_interval 25 --add_log_row_interval 25
