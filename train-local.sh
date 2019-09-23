b9225bcaa81806ffd1ded37ee65e6c75e686cc4e
Chenghao <mouchenghao@gmail.com>
2019-09-23 14:08:06 -0700

    add loss check

--- train-local.sh
+++ train-local.sh
@@ -1,4 +1,4 @@
-PYTHON=/Users/chenghaomou/Anaconda/envs/Elisa/bin/python
+PYTHON=/auto/nlg-05/chengham/anaconda3/envs/py37/bin/python
 TRAIN=train.py
 
 $PYTHON $TRAIN --model_type $1 --model_weight $2 \
@@ -8,4 +8,5 @@ $PYTHON $TRAIN --model_type $1 --model_weight $2 \
   --task_cache_dir ./cache \
   --output_dir output/$3-$1-$2-pred \
   --val_check_interval 0.01 \
-  --log_save_interval 5 --add_log_row_interval 5
+  --log_save_interval 5 --add_log_row_interval 5 \
+  --fast_dev_run
