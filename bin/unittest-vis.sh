2354edcbe2e5449381becaa4255757716cf45293
Chenghao <mouchenghao@gmail.com>
2019-10-22 19:34:39 -0700

    add gradient viz

--- bin/unittest-vis.sh
+++ bin/unittest-vis.sh
@@ -2,15 +2,15 @@
 PYTHON=/Users/chenghaomou/Anaconda/envs/Elisa/bin/python
 EVAL=gradient_visual.py
 
-$PYTHON -W ignore $EVAL --model_type distilbert \
-  --model_weight distilbert-base-uncased \
-  --task_name physicaliqa \
+$PYTHON -W ignore $EVAL --model_type $1 \
+  --model_weight $2 \
+  --task_name $3 \
   --task_config_file config/tasks.yaml \
   --task_cache_dir ./cache \
   --running_config_file config/hyparams.yaml \
-  --test_input_dir ./cache/physicaliqa-test-input \
-  --output_dir output/distilbert-distilbert-base-uncased-physicaliqa-test-pred \
-  --weights_path output/distilbert-distilbert-base-uncased-checkpoints/physicaliqa/0/_ckpt_epoch_5.ckpt \
-  --tags_csv output/distilbert-distilbert-base-uncased-log/physicaliqa/version_0/meta_tags.csv \
+  --test_input_dir ./cache/$3-test-input \
+  --output_dir output/$1-$2-$3-test-pred \
+  --weights_path output/$1-$2-checkpoints/$3/0/_ckpt_epoch_5.ckpt \
+  --tags_csv output/$1-$2-log/$3/version_0/meta_tags.csv \
   --embedding_layer encoder.model.embeddings.word_embeddings \
   --output visualization.html
