c649d565ab4ae1fa400d8c0a6cd190074991331e
Chenghao <mouchenghao@gmail.com>
2019-10-22 20:00:07 -0700

    fix test script bug

--- bin/unittest-vis.sh
+++ bin/unittest-vis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PYTHON=/Users/chenghaomou/Anaconda/envs/Elisa/bin/python
+PYTHON=/auto/nlg-05/chengham/anaconda3/envs/py37/bin/python
 EVAL=gradient_visual.py
 
 $PYTHON -W ignore $EVAL --model_type $1 \
@@ -10,7 +10,7 @@ $PYTHON -W ignore $EVAL --model_type $1 \
   --running_config_file config/hyparams.yaml \
   --test_input_dir ./cache/$3-test-input \
   --output_dir output/$1-$2-$3-test-pred \
-  --weights_path output/$1-$2-checkpoints/$3/0/_ckpt_epoch_5.ckpt \
+  --weights_path output/$1-$2-checkpoints/$3/0/_ckpt_epoch_3.ckpt \
   --tags_csv output/$1-$2-log/$3/version_0/meta_tags.csv \
   --embedding_layer encoder.model.embeddings.word_embeddings \
   --output visualization.html
