bcfc05166754b7a0ba32d74abbee17c494931e3d
Chenghao <mouchenghao@gmail.com>
2019-11-23 19:57:21 -0800

    update scripts

--- submit/anli.sh
+++ submit/anli.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # PYTHON=/auto/nlg-05/chengham/anaconda3/envs/py37/bin/python
 PYTHON=python
-$EVAL=test.py
+EVAL=test.py
 
 MODEL=roberta
 MODEL_WEIGHT=roberta-large
@@ -16,4 +16,4 @@ $PYTHON -W ignore $EVAL --model_type $MODEL \
   --test_input_dir $1 \
   --output_dir $2 \
   --weights_path output/$MODEL-$MODEL_WEIGHT-checkpoints/$TASK/0/_ckpt_epoch_3.ckpt \
-  --tags_csv output/$MODEL-$MODEL_WEIGHT-log/$TASK/version_0/meta_tags.csv
+  --tags_csv output/$MODEL-$MODEL_WEIGHT-log/$TASK/meta_tags.csv
