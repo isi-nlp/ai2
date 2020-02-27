02118163ed232b2904b3d33d15aae27682876926
Deniz Beser <denizbeser15@gmail.com>
2020-02-27 16:27:42 -0500

    fixup saga eval script

--- bin/eval_saga.sh
+++ bin/eval_saga.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
-#PYTHON=/auto/nlg-05/chengham/anaconda3/envs/py37/bin/python
-PYTHON=python
+
+#SBATCH --account=mics
+#SBATCH --partition=mics
+#SBATCH --ntasks=1
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus-per-task=1
+#SBATCH --mem-per-cpu=4g
+
 source ~/.bashrc
 conda activate ai2
 
+. /scratch/spack/share/spack/setup-env.sh
+# When using `tensorflow-gpu`, paths to CUDA and CUDNN libraries are required
+# by symbol lookup at runtime even if a GPU isn't going to be used.
+spack load cuda@9.0.176
+spack load cudnn@7.6.5.32-9.0-linux-x64
+
+PYTHON=python
+#MODEL_TYPE=$1
+#MODEL_WEIGHT=$2
+#TASK_NAME=$3
+EXPERIMENT_NAME=$4
 EVAL=eval.py
 
-OUTPUT=output
+
+OUTPUT=output/${EXPERIMENT_NAME}/
 FILE=$OUTPUT/$1-$2-checkpoints/$3/0/_ckpt_epoch_6.ckpt
 if [ ! -f "$FILE" ]; then
   FILE=$OUTPUT/$1-$2-checkpoints/$3/0/_ckpt_epoch_5.ckpt
