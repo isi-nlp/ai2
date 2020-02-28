fa51f92431c1fdefce75e429e3a328350f34b72f
Deniz Beser <denizbeser15@gmail.com>
2020-02-27 19:34:27 -0500

    task fixup

--- bin/run_experiments.sh
+++ bin/run_experiments.sh
@@ -1,7 +1,7 @@
 MODEL_TYPE="roberta"
 MODEL_WEIGHTS="roberta-large"
 
-declare -a arr1=("physicaliqa" "physicaliqa-25pc" "physicaliqa-carved" "physicaliqa-carved-25pc")
+declare -a arr1=("physicaliqa" "physicaliqa-carved") #"physicaliqa-25pc" "physicaliqa-carved-25pc")
 declare -a arr2=("cn_all_cs_10k" "cn_all_cs_20k" "cn_physical_10k" "cn_carved_10k")
 
 ## now loop through the above task arrays
