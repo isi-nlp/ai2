ec7ac1afbece37b024e37f092cd886dfd8d0c53e
Deniz Beser <denizbeser15@gmail.com>
2020-03-03 10:16:53 -0500

    revert to working version

--- bin/run_experiments.sh
+++ bin/run_experiments.sh
@@ -1,15 +1,15 @@
 MODEL_TYPE="roberta"
 MODEL_WEIGHTS="roberta-large"
 
-declare -a arr1=("physicaliqa") #"physicaliqa-carved" #"physicaliqa-25pc" "physicaliqa-carved-25pc")
-declare -a arr2=("cn_physical_10k" "cn_carved_10k") # "cn_all_cs_10k" "cn_all_cs_20k" )
+declare -a arr1=("physicaliqa" "physicaliqa-carved" "physicaliqa-25pc" "physicaliqa-carved-25pc")
+declare -a arr2=("cn_physical_10k" "cn_carved_10k" "cn_all_cs_10k" "cn_all_cs_20k" )
 
 ## now loop through the above task arrays
 for TASK1 in "${arr1[@]}"
 do
   for TASK2 in "${arr2[@]}"
   do
-    EXP_NAME="${MODEL_TYPE}-${TASK1}-${TASK2}";
+    EXP_NAME="${MODEL_WEIGHTS}-${TASK1}-${TASK2}";
     sbatch -J "$EXP_NAME" -o "${EXP_NAME}.out" "bin/train_saga_double_task.sh" "$MODEL_TYPE" "$MODEL_WEIGHTS" "$TASK1" "$TASK2" "$EXP_NAME"
   done
 done
