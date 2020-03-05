fbdf576ddd55df2fd677f4dcf24c1767355a8da6
Deniz Beser <denizbeser15@gmail.com>
2020-03-05 09:21:04 -0500

    10pc and cn 40k yaml and scripts

--- bin/run_experiments.sh
+++ bin/run_experiments.sh
@@ -1,8 +1,8 @@
 MODEL_TYPE="roberta"
 MODEL_WEIGHTS="roberta-large"
 
-declare -a arr1=("physicaliqa" "physicaliqa-carved" "physicaliqa-25pc" "physicaliqa-carved-25pc")
-declare -a arr2=("cn_physical_10k" "cn_carved_10k" "cn_all_cs_10k" "cn_all_cs_20k" )
+declare -a arr1=("physicaliqa-25pc")
+declare -a arr2=("cn_physical_10k" "cn_all_cs_10k" "cn_all_cs_20k" "cn_all_cs_40k")
 
 ## now loop through the above task arrays
 for TASK1 in "${arr1[@]}"
