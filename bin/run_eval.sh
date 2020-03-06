f25fcf8e87cfe9b10d33c8ee59476cfa268251d2
Deniz Beser <denizbeser15@gmail.com>
2020-03-06 11:58:48 -0500

    cn second datasets with random seed change

--- bin/run_eval.sh
+++ bin/run_eval.sh
@@ -1,8 +1,9 @@
 MODEL_TYPE="roberta"
 MODEL_WEIGHTS="roberta-large"
 
-declare -a experiments=("physicaliqa-10pc,cn_physical_10k" "physicaliqa-10pc,cn_all_cs_10k" "physicaliqa-10pc,cn_all_cs_20k" \
-                        "physicaliqa-10pc,cn_all_cs_40k" "physicaliqa-25pc,cn_all_cs_40k" "physicaliqa,cn_all_cs_40k")
+declare -a experiments=("physicaliqa-10pc,cn_physical_10k" "physicaliqa-25pc,cn_all_cs_10k" "physicaliqa,cn_all_cs_10k" \
+                        "physicaliqa-10pc,cn_all_cs_20k_2" "physicaliqa-25pc,cn_all_cs_20k_2" "physicaliqa,cn_all_cs_20k_2" \
+                        "physicaliqa-10pc,cn_all_cs_40k_2" "physicaliqa-25pc,cn_all_cs_40k_2" "physicaliqa,cn_all_cs_40k_2")
 
 for i in "${experiments[@]}"; do
     IFS=',' read TASK1 TASK2 <<< "${i}"
@@ -12,11 +13,11 @@ for i in "${experiments[@]}"; do
     grep 'confidence' 'eval.out' | tail -1
 done
 
-declare -a experiments=("physicaliqa-10pc" "physicaliqa-25pc")
+#declare -a experiments=("physicaliqa-10pc" "physicaliqa-25pc")
 
-for TASK1 in "${experiments[@]}"; do
-    EXP_NAME="${MODEL_WEIGHTS}-${TASK1}";
-    echo $EXP_NAME | tee -a 'eval.out'
-    sh 'bin/eval_saga.sh' $MODEL_TYPE $MODEL_WEIGHTS $TASK1 $EXP_NAME &>> 'eval.out'
-    grep 'confidence' 'eval.out' | tail -1
-done
+#for TASK1 in "${experiments[@]}"; do
+#    EXP_NAME="${MODEL_WEIGHTS}-${TASK1}";
+#    echo $EXP_NAME | tee -a 'eval.out'
+#    sh 'bin/eval_saga.sh' $MODEL_TYPE $MODEL_WEIGHTS $TASK1 $EXP_NAME &>> 'eval.out'
+#    grep 'confidence' 'eval.out' | tail -1
+#done
--- bin/run_experiments.sh
+++ bin/run_experiments.sh
@@ -1,8 +1,9 @@
 MODEL_TYPE="roberta"
 MODEL_WEIGHTS="roberta-large"
 
-declare -a experiments=("physicaliqa-10pc,cn_physical_10k" "physicaliqa-10pc,cn_all_cs_10k" "physicaliqa-10pc,cn_all_cs_20k" \
-                        "physicaliqa-10pc,cn_all_cs_40k" "physicaliqa-25pc,cn_all_cs_40k" "physicaliqa,cn_all_cs_40k")
+declare -a experiments=("physicaliqa-10pc,cn_physical_10k" "physicaliqa-25pc,cn_all_cs_10k" "physicaliqa,cn_all_cs_10k" \
+                        "physicaliqa-10pc,cn_all_cs_20k_2" "physicaliqa-25pc,cn_all_cs_20k_2" "physicaliqa,cn_all_cs_20k_2" \
+                        "physicaliqa-10pc,cn_all_cs_40k_2" "physicaliqa-25pc,cn_all_cs_40k_2" "physicaliqa,cn_all_cs_40k_2")
 
 for i in "${experiments[@]}"; do
     IFS=',' read TASK1 TASK2 <<< "${i}"
@@ -11,10 +12,10 @@ for i in "${experiments[@]}"; do
     sbatch -J "$EXP_NAME" -o "${EXP_NAME}.out" "bin/train_saga_double_task.sh" "$MODEL_TYPE" "$MODEL_WEIGHTS" "$TASK1" "$TASK2" "$EXP_NAME"
 done
 
-declare -a experiments=("physicaliqa-10pc" "physicaliqa-25pc")
-
-for TASK1 in "${experiments[@]}"; do
-    EXP_NAME="${MODEL_WEIGHTS}-${TASK1}";
-    echo "${TASK1}"
-    sbatch -J "$EXP_NAME" -o "${EXP_NAME}.out" "bin/train_saga.sh" "$MODEL_TYPE" "$MODEL_WEIGHTS" "$TASK1" "$EXP_NAME"
-done
+#declare -a experiments=("physicaliqa-10pc" "physicaliqa-25pc")
+#
+#for TASK1 in "${experiments[@]}"; do
+#    EXP_NAME="${MODEL_WEIGHTS}-${TASK1}";
+#    echo "${TASK1}"
+#    sbatch -J "$EXP_NAME" -o "${EXP_NAME}.out" "bin/train_saga.sh" "$MODEL_TYPE" "$MODEL_WEIGHTS" "$TASK1" "$EXP_NAME"
+#done
