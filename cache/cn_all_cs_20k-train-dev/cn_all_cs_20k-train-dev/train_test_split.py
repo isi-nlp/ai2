4a98c5c6da7acddf3439632ecb7d8d6b7a4fbc4b
Deniz Beser <denizbeser15@gmail.com>
2020-03-23 16:37:58 -0400

    cnqa datasets

---
+++ cache/cn_all_cs_20k-train-dev/cn_all_cs_20k-train-dev/train_test_split.py
@@ -0,0 +1,57 @@
+import json
+import random as r
+
+f = open("cn-all-cs-multiple-choice-data.jsonl", "r")
+l = open("cn-all-cs-multiple-choice-labels.lst", "r")
+
+all_lines = []
+all_labs = []
+
+for line in f:
+    all_lines.append(line)
+
+for lab in l:
+    all_labs.append(lab)
+
+assert len(all_lines) == len(all_labs)
+
+r.seed(0)
+c = list(zip(all_lines, all_labs))
+r.shuffle(c)
+all_lines, all_labs = zip(*c)
+
+eval_num = 2000
+train_num = 20000
+
+train_lines = all_lines[eval_num:eval_num+train_num]
+train_labs = all_labs[eval_num:eval_num+train_num]
+eval_lines = all_lines[:eval_num]
+eval_labs = all_labs[:eval_num]
+
+assert len(train_lines) == len(train_labs)
+assert len(eval_lines) == len(eval_labs)
+
+print ("Number of data in train: {}".format(len(train_lines)))
+print ("Number of data in eval:  {}".format(len(eval_lines)))
+
+ft = open("train.jsonl", "w")
+lt = open("train-labels.lst", "w")
+fe = open("dev.jsonl", "w")
+le = open("dev-labels.lst", "w")
+
+for line in train_lines:
+    ft.write(line)
+for lab in train_labs:
+    lt.write(lab)
+
+for line in eval_lines:
+    fe.write(line)
+for lab in eval_labs:
+    le.write(lab)
+
+ft.close()
+lt.close()
+fe.close()
+le.close()
+f.close()
+l.close()
---
+++ cache/physicaliqa-10pc-train-dev/physicaliqa-10pc-train-dev/train_test_split.py
@@ -0,0 +1,37 @@
+import json
+
+f = open("train.jsonl", "r")
+l = open("train-labels.lst", "r")
+
+all_lines = []
+all_labs = []
+
+for line in f:
+    all_lines.append(line)
+
+for lab in l:
+    all_labs.append(lab)
+
+assert len(all_lines) == len(all_labs)
+
+train_num = int(float(len(all_lines)) * 0.40)
+
+train_lines = all_lines[:train_num]
+train_labs = all_labs[:train_num]
+
+assert len(train_lines) == len(train_labs)
+
+print ("Number of data in train: {}".format(len(train_lines)))
+
+ft = open("train.jsonl", "w")
+lt = open("train-labels.lst", "w")
+
+for line in train_lines:
+    ft.write(line)
+for lab in train_labs:
+    lt.write(lab)
+
+ft.close()
+lt.close()
+f.close()
+l.close()
