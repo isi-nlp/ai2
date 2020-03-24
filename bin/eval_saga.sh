5bf2d51d591101781a380a0b27db9c722a07eafa
Deniz Beser <denizbeser15@gmail.com>
2020-03-24 10:06:07 -0400

    eval scripts

---
+++ bin/eval_saga.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#SBATCH --account=mics
+#SBATCH --partition=mics
+#SBATCH --ntasks=1
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus-per-task=1
+#SBATCH --mem-per-cpu=4g
+
+source ~/.bashrc
+conda activate ai2
+
+. /scratch/spack/share/spack/setup-env.sh
+# When using `tensorflow-gpu`, paths to CUDA and CUDNN libraries are required
+# by symbol lookup at runtime even if a GPU isn't going to be used.
+spack load cuda@9.0.176
+spack load cudnn@7.6.5.32-9.0-linux-x64
+
+EXPERIMENT_NAME=$1
+CONF=$2
+
+OUTPUT=output/${EXPERIMENT_NAME}/
+#FILE=$OUTPUT/$1-$2-checkpoints/$3/0/_ckpt_epoch_6.ckpt
+#if [ ! -f "$FILE" ]; then
+#  FILE=$OUTPUT/$1-$2-checkpoints/$3/0/_ckpt_epoch_5.ckpt
+#  if [ ! -f "$FILE" ]; then
+#    FILE=$OUTPUT/$1-$2-checkpoints/$3/0/_ckpt_epoch_3.ckpt
+#    if [ ! -f "$FILE" ]; then
+#      FILE=$OUTPUT/$1-$2-checkpoints/$3/0/_ckpt_epoch_2.ckpt
+if [ ! -f "$FILE" ]; then
+  FILE=$OUTPUT/checkpoints/_ckpt_epoch_4.ckpt
+fi
+#    fi
+#  fi
+#fi
+
+python eval.py \
+  --input_x cache/physicaliqa-train-dev/physicaliqa-train-dev/dev.jsonl \
+  --config "$CONF" \
+  --checkpoint "$FILE" \
+  --input_y cache/physicaliqa-train-dev/physicaliqa-train-dev/dev-labels.lst \
+  --output pred.lst
---
+++ bin/run_eval.sh
@@ -0,0 +1,27 @@
+MODEL_TYPE="roberta"
+MODEL_WEIGHTS="roberta-large"
+
+declare -a experiments=("physicaliqa-10pc,cn_all_cs_10k" "physicaliqa-25pc,cn_all_cs_10k" "physicaliqa,cn_all_cs_10k" \
+                        "physicaliqa-10pc,cn_all_cs_20k" "physicaliqa-25pc,cn_all_cs_20k" "physicaliqa,cn_all_cs_20k" \
+                        "physicaliqa-10pc,cn_all_cs_40k" "physicaliqa-25pc,cn_all_cs_40k" "physicaliqa,cn_all_cs_40k" \
+                        "physicaliqa-10pc,cn_physical_10k" "physicaliqa-25pc,cn_physical_10k" "physicaliqa,cn_physical_10k")
+
+for i in "${experiments[@]}"; do
+    IFS=',' read TASK1 TASK2 <<< "${i}"
+    EXP_NAME="${MODEL_WEIGHTS}-${TASK1}-${TASK2}";
+    CONF="configs/${TASK1}-${TASK2}.yaml"
+    echo "$EXP_NAME" | tee -a 'eval.out'
+    sh 'bin/eval_saga.sh' "$EXP_NAME" "$CONF" &>> 'eval.out'
+    grep 'confidence' 'eval.out' | tail -1 | tee -a 'eval_results.out'
+done
+
+declare -a experiments=("physicaliqa-10pc" "physicaliqa-25pc" "physicaliqa")
+
+for TASK1 in "${experiments[@]}"; do
+    EXP_NAME="${MODEL_WEIGHTS}-${TASK1}";
+    CONF="configs/${TASK1}.yaml"
+    echo "$EXP_NAME" | tee -a 'eval.out'
+    sh 'bin/eval_saga.sh' "$EXP_NAME" "$CONF" &>> 'eval.out'
+    grep 'confidence' 'eval.out' | tail -1
+    grep 'confidence' 'eval.out' | tail -1 | tee -a 'eval_results.out'
+done
