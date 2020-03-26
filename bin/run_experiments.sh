4bae70c494b4bd78fe365b6e767604ffbbc10496
Deniz Beser <denizbeser15@gmail.com>
2020-03-26 14:40:46 -0400

    seed setup

--- bin/run_experiments.sh
+++ bin/run_experiments.sh
@@ -1,5 +1,6 @@
 MODEL_TYPE="roberta"
 MODEL_WEIGHTS="roberta-large"
+SEED=$1
 
 declare -a experiments=("physicaliqa-10pc,cn_all_cs_10k" "physicaliqa-25pc,cn_all_cs_10k" "physicaliqa,cn_all_cs_10k" \
                         "physicaliqa-10pc,cn_all_cs_20k" "physicaliqa-25pc,cn_all_cs_20k" "physicaliqa,cn_all_cs_20k" \
@@ -8,7 +9,7 @@ declare -a experiments=("physicaliqa-10pc,cn_all_cs_10k" "physicaliqa-25pc,cn_al
 
 for i in "${experiments[@]}"; do
     IFS=',' read TASK1 TASK2 <<< "${i}"
-    EXP_NAME="${TASK1}-${TASK2}";
+    EXP_NAME="${TASK1}-${TASK2}-${SEED}";
     echo "${TASK1}" and "${TASK2}"
     CONF="configs/${TASK1}-${TASK2}.yaml"
     sbatch -J "$EXP_NAME" -o "${EXP_NAME}.out" "bin/run_saga.sh" "$CONF"
@@ -17,7 +18,7 @@ done
 declare -a experiments=("physicaliqa-10pc" "physicaliqa-25pc" "physicaliqa")
 
 for TASK1 in "${experiments[@]}"; do
-    EXP_NAME="${TASK1}";
+    EXP_NAME="${TASK1}-${SEED}";
     echo "${TASK1}"
     CONF="configs/${TASK1}.yaml"
     sbatch -J "$EXP_NAME" -o "${EXP_NAME}.out" "bin/run_saga.sh" "$CONF"
