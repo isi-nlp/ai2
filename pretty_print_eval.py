ea573c33dfb4ea78c75a9c7692ea67ad2750fd10
Deniz Beser <denizbeser15@gmail.com>
2020-03-27 12:01:52 -0400

    eval.py

--- pretty_print_eval.py
+++ pretty_print_eval.py
@@ -1,8 +1,10 @@
 with open('eval_results-10061880.out') as eval:
     lines = eval.readlines()
-    for i in range(0, len(lines), 2):
+    for i in range(0, len(lines), 3):
         experiment = lines[i].strip()
-        result_tokens = lines[i+1].strip().split()
+        f1 = lines[i+2].strip().split()[-1]
+        result_tokens = lines[i+2].strip().split()
         low, high, ave = result_tokens[-5], result_tokens[-3].strip(','), result_tokens[-1]
         print(experiment[experiment.index('-',10)+1:])
+        print(f1)
         print(f"{ave} {{{low} - {high}}}")
