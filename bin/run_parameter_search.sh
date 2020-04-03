195d438775774c603ea85b6c7cf018a7f9a94c8f
Deniz Beser <denizbeser15@gmail.com>
2020-04-03 12:19:10 -0400

    restructuring sweep scripts

---
+++ bin/run_parameter_search.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#SBATCH --partition=ephemeral
+#SBATCH --qos=ephemeral
+
+#SBATCH --ntasks=1
+#SBATCH --time=20:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus-per-task=1
+#SBATCH --mem-per-cpu=4g
+
+#SBATCH --output=param_search.out
+#SBATCH --array=0-143%18
+
+source ~/.bashrc
+conda activate ai2
+
+. /scratch/spack/share/spack/setup-env.sh
+# When using `tensorflow-gpu`, paths to CUDA and CUDNN libraries are required
+# by symbol lookup at runtime even if a GPU isn't going to be used.
+spack load cuda@9.0.176
+spack load cudnn@7.6.5.32-9.0-linux-x64
+
+ARGS=$(head -n 1 "sweep_args/args_${SLURM_ARRAY_TASK_ID}.txt")
+echo "Running task ${SLURM_ARRAY_TASK_ID}"
+
+python train.py "${1}"
---
+++ generate_sweep_args.py
@@ -0,0 +1,36 @@
+MODEL_TYPE="roberta"
+MODEL_WEIGHTS="roberta-large"
+SEED=10061880
+
+double_experiments=[("physicaliqa-10pc","cn_all_cs_20k"), ("physicaliqa","cn_all_cs_20k")]
+single_experiments=["physicaliqa-10pc", "physicaliqa"]
+
+all_args = []
+
+# Sweep parameters
+for BS in [3, 4]:
+    for ACB in [2, 8, 16]:
+        for WS in [150, 300]:
+            for DR in ['0', '0.3', '0.5']:
+
+                # For each parameter setup, prepate arguments file
+                SWEEP_PARAMS=f"--accumulate_grad_batches {ACB} --batch_size {BS} --warmup_steps {WS}--dropout {DR} --random_seed {SEED}"
+                SETUP_NAME=f"rs{SEED}_bs{BS}_acb{ACB}_ws{WS}_dr{DR}"
+
+                # Train double task experiments
+                for TASK1, TASK2 in double_experiments:
+                    # Setup all parameters
+                    DIF_PARAMS=f"--config_file configs/{TASK1}-{TASK2}.yaml --save_path outputs/{SETUP_NAME}/{MODEL_WEIGHTS}-{TASK1}-{TASK2}"
+                    PARAMS=f"{DIF_PARAMS} {SWEEP_PARAMS}"
+                    all_args.append(PARAMS)
+
+                # Train single task experiments
+                for TASK1 in single_experiments:
+                    # Setup all parameters
+                    DIF_PARAMS=f"--config_file configs/{TASK1}.yaml  --save_path outputs/{SETUP_NAME}/{MODEL_WEIGHTS}-{TASK1}"
+                    PARAMS=f"{DIF_PARAMS} {SWEEP_PARAMS}"
+                    all_args.append(PARAMS)
+
+for i, args in enumerate(all_args):
+    with open(f'sweep_args/args_{i}.txt', 'w') as args_file:
+        args_file.write(args)
