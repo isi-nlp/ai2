54a978349d4586e701b99eeb6f5a9d0a3dd1a4da
David Li <w245li@edu.uwaterloo.ca>
2020-04-07 00:58:17 -0300

    finished incremental training job

--- train.py
+++ train.py
@@ -1,13 +1,17 @@
 import os
 import random
+from typing import *
 
 import hydra
 import numpy as np
 import torch
+import torch.nn.functional as F
 from loguru import logger
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 from test_tube import Experiment
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from model import Classifier
 
@@ -64,6 +68,35 @@ def train(config):
         experiment=exp,
     )
     trainer.fit(model)
+
+    # eval
+
+    device = 'cpu' if not torch.cuda.is_available() else "cuda"
+    model.to(device)
+    model.eval()
+
+    preds: List[int] = []
+    proba: List[List[float]] = []
+    for batch in tqdm(DataLoader(model.dataloader(
+            "../../../" + config["val_x"],
+            "../../../" + config["val_y"]),
+            batch_size=model.hparams["batch_size"] * 2,
+            collate_fn=model.collate,
+            shuffle=False)):
+
+        for key in batch:
+            if isinstance(batch[key], torch.Tensor):
+                batch[key] = batch[key].to(device)
+
+        with torch.no_grad():
+            logits = model.forward(batch)
+        preds.extend(torch.argmax(logits, dim=1).cpu().detach().numpy().tolist())
+        proba.extend(F.softmax(logits, dim=-1).cpu().detach().numpy().tolist())
+    preds = [p + model.label_offset for p in preds]
+    with open(f"{save_path}/preds.lst", "w") as f:
+        f.write("\n".join(map(str, preds)))
+    with open("{save_path}/proba.lst", "w") as f:
+        f.write("\n".join(map(lambda l: '\t'.join(map(str, l)), proba)))
 
 
 if __name__ == "__main__":
