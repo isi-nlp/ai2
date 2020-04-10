7f2fd66c080ae0bf63446b4f9cb9346f89b540ff
Deniz Beser <denizbeser15@gmail.com>
2020-04-10 10:16:59 -0400

    analysis of 10k param search

--- analyze_csv.py
+++ analyze_csv.py
@@ -6,9 +6,10 @@ pd.set_option('display.width', None)
 df = pd.read_csv('param_search.csv')
 
 for d1 in (df['AI2_Data'].unique()):
-    for d2 in (df['CN_Data'].unique()):
-        sub_df = (df[(df['AI2_Data']==d1) & (df['CN_Data']==d2)]).sort_values(by=['accuracy'], ascending=False)
-        top = sub_df[["AI2_Data", "CN_Data", "bs", "acb", "ws", "dr", "accuracy"]].head(10)
+    sub_df = df[(df['AI2_Data']==d1)]
+    for d2 in (sub_df['CN_Data'].unique()):
+        sorted_df = (sub_df[sub_df['CN_Data']==d2]).sort_values(by=['accuracy'], ascending=False)
+        top = sorted_df[["AI2_Data", "CN_Data", "bs", "acb", "ws", "dr", "accuracy"]].head(10)
         top.reset_index(drop=True, inplace=True)
         with open("top_10.txt", "a") as out:
             print(top, file=out)
