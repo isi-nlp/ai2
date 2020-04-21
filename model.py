d0771d2cd0dfc9381feec040e4c32ff4ea270d88
Deniz Beser <denizbeser15@gmail.com>
2020-04-21 14:07:05 -0400

    t5

--- model.py
+++ model.py
@@ -145,9 +145,9 @@ class Classifier(pl.LightningModule):
         batch["token_type_ids"] = None if "roberta" in self.hparams["model"] or "lm_finetuned" \
                                           in self.hparams["model"] else batch["token_type_ids"]
 
-        print(batch)
-        results = self.embedder(input_ids=batch["input_ids"]
-                                )
+        print(self.embedder)
+        results = self.embedder(input_ids=batch["input_ids"],
+                                decoder_input_ids=batch["input_ids"],)
 
         token_embeddings, *_ = results
         output = torch.mean(token_embeddings, dim=1).squeeze()
