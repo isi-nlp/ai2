29cdd6a645d2a2d00d128451a8ab507ffcdc55a9
Deniz Beser <denizbeser15@gmail.com>
2020-05-12 18:36:21 -0400

    analyses

--- compare_predictions.py
+++ compare_predictions.py
@@ -4,13 +4,13 @@ import pandas as pd
 
 versions_to_predictions = {
     'standard_rs0': "outputs/roberta-large-physicaliqa_rs0/roberta-large-physicaliqa",
-    'standard_rs42': "outputs/roberta-large-physicaliqa_rs42/roberta-large-physicaliqa",
+    # 'standard_rs42': "outputs/roberta-large-physicaliqa_rs42/roberta-large-physicaliqa",
     'standard_rs10061880': "outputs/roberta-large-physicaliqa",
     'arc1_rs0': 'outputs/roberta-large_rs0_acb2_lr5e-6/roberta-large-physicaliqa-arc1',
-    'arc1_rs42': 'outputs/roberta-large_rs42_acb4_lr5e-6/roberta-large-physicaliqa-arc1',
+    # 'arc1_rs42': 'outputs/roberta-large_rs42_acb4_lr5e-6/roberta-large-physicaliqa-arc1',
     'arc1_rs10061880': 'outputs/roberta-large_rs10061880_acb2_lr5e-6/roberta-large-physicaliqa-arc1',
     'arc2_rs0': 'outputs/roberta-large_rs0_acb1_lr1e-6/roberta-large-physicaliqa-arc2',
-    'arc2_rs42': 'outputs/roberta-large_rs42_acb8_lr5e-6/roberta-large-physicaliqa-arc2',
+    # 'arc2_rs42': 'outputs/roberta-large_rs42_acb8_lr5e-6/roberta-large-physicaliqa-arc2',
     'arc2_rs10061880': 'outputs/roberta-large_rs10061880_acb1_lr5e-6/roberta-large-physicaliqa-arc2',
 }
 
