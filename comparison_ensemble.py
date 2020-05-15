1fd1b96abfbcb6b23e31f534878c80632e47cde5
Deniz Beser <denizbeser15@gmail.com>
2020-05-15 14:01:28 -0400

    confidence

--- comparison_ensemble.py
+++ comparison_ensemble.py
@@ -53,7 +53,7 @@ for id1, id2 in itertools.combinations(model_to_predictions.keys(), 2):
 subset = ['standard_rs0', 'standard_rs10061880']
 # Run ensemble
 predictions_df = (pd.DataFrame.from_dict(model_to_predictions)-0.5)*2
-confidences_df = pd.DataFrame.from_dict(model_to_confidences)
+confidences_df = pd.DataFrame.from_dict(model_to_confidences) > 0.3
 scaled_df = predictions_df.mul(confidences_df, fill_value=1)[subset]
 print(predictions_df)
 print(confidences_df)
