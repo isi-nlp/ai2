b6e674813e333ec6d230084f6168373bac452ff8
Deniz Beser <denizbeser15@gmail.com>
2020-05-15 13:55:22 -0400

    confidence

--- comparison_ensemble.py
+++ comparison_ensemble.py
@@ -50,9 +50,14 @@ for id1, id2 in itertools.combinations(model_to_predictions.keys(), 2):
 
     print(f'{id1},{id2},{accuracy_score(preds1, preds2)},{pearsonr(preds1, preds2)[0]},{pearsonr(correctness1, correctness2)[0]},{pearsonr(conf1, conf2)[0]},{ccbc},{ccoc},{ccbw}')
 
+subset = ['standard_rs0', 'standard_rs10061880']
 # Run ensemble
 predictions_df = pd.DataFrame.from_dict(model_to_predictions)
 confidences_df = pd.DataFrame.from_dict(model_to_confidences)
-mean = predictions_df.mean(axis = 1)
+scaled_df = predictions_df.mul[confidences_df]
+print(predictions_df)
+print(confidences_df)
+print(scaled_df)
+mean = scaled_df.mean(axis = 1)
 final = mean > 0
 print(accuracy_score(labels, final.values.squeeze().tolist()))
