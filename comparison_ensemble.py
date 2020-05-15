79f30fc33a0774df13b1d96bdacd0a6b3c8aa4f2
Deniz Beser <denizbeser15@gmail.com>
2020-05-15 12:35:33 -0400

    confidence

--- comparison_ensemble.py
+++ comparison_ensemble.py
@@ -3,7 +3,7 @@ from sklearn.metrics import accuracy_score
 import pandas as pd
 from scipy.stats.stats import pearsonr
 
-versions_to_predictions = {
+model_to_path = {
     'standard_rs0': "outputs/roberta-large-physicaliqa_rs0/roberta-large-physicaliqa",
     # 'standard_rs42': "outputs/roberta-large-physicaliqa_rs42/roberta-large-physicaliqa",
     'standard_rs10061880': "outputs/roberta-large-physicaliqa",
@@ -14,30 +14,33 @@ versions_to_predictions = {
     # 'arc2_rs42': 'outputs/roberta-large_rs42_acb8_lr5e-6/roberta-large-physicaliqa-arc2',
     'arc2_rs10061880': 'outputs/roberta-large_rs10061880_acb1_lr5e-6/roberta-large-physicaliqa-arc2',
 }
+model_to_predictions = {}
+model_to_confidences = {}
 
 gold_labels_path = 'cache/physicaliqa-train-dev/physicaliqa-train-dev/dev-labels.lst'
 labels = pd.read_csv(gold_labels_path, sep='\t', header=None).values.tolist()
 
 # Check accuracy of each model
-for key in versions_to_predictions.keys():
+for key, path in model_to_path.items():
     print('Accuracy of each model:')
-    preds = pd.read_csv(versions_to_predictions[key]+'/pred.lst', sep='\t', header=None).values.tolist()
+    preds = pd.read_csv(path+'/pred.lst', sep='\t', header=None).values.tolist()
+    confs = pd.read_csv(path+'/pred.lst.cnf', sep='\t', header=None).values.tolist()
     accuracy = accuracy_score(labels, preds)
+    model_to_predictions[key] = preds
+    model_to_confidences[key] = confs
     print(f'{key},{accuracy}')
 
 # Compare pairs of predictions of each model
 print('ID1,ID22,Prediction Sim,Prediction Cor,Correctness Sim,Correctness Cor,Confidence Cor')
-for id1, id2 in itertools.combinations(versions_to_predictions.keys(), 2):
+for id1, id2 in itertools.combinations(model_to_predictions.keys(), 2):
     print(f'Pairwise comparison of {id1} and {id2}')
     model1, rs1 = tuple(id1.split('_'))
     model2, rs2 = tuple(id2.split('_'))
     if model1 != model2 and rs1 != rs2: continue # skip if both the model and rs are different
 
-    preds1 = pd.read_csv(versions_to_predictions[id1]+'/pred.lst', sep='\t', header=None).values.tolist()
-    conf1 = pd.read_csv(versions_to_predictions[id1]+'/pred.lst.cnf', sep='\t', header=None).values.tolist()
+    preds1, conf1 = model_to_predictions[id1], model_to_confidences[id1]
     correctness1 = [int(p == labels[i]) for i, p in enumerate(preds1)]
-    preds2 = pd.read_csv(versions_to_predictions[id2]+'/pred.lst', sep='\t', header=None).values.tolist()
-    conf2 = pd.read_csv(versions_to_predictions[id2]+'/pred.lst.cnf', sep='\t', header=None).values.tolist()
+    preds2, conf2 = model_to_predictions[id2], model_to_confidences[id2]
     correctness2 = [int(p == labels[i]) for i, p in enumerate(preds2)]
 
     print(f'{id1},{id2},{accuracy_score(preds1, preds2)},{pearsonr(preds1, preds2)[0]}\
