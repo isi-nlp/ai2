be2a63493aa9965b7e95cdd66cfe1c29970ec38e
Deniz Beser <denizbeser15@gmail.com>
2020-05-18 16:58:45 -0400

    ensembling

--- comparison_ensemble.py
+++ comparison_ensemble.py
@@ -1,4 +1,6 @@
 import itertools
+
+from more_itertools import powerset
 from sklearn.metrics import accuracy_score
 import pandas as pd
 from scipy.stats.stats import pearsonr
@@ -10,9 +12,9 @@ model_to_path = {
     'arc1_rs0': 'outputs/roberta-large_rs0_acb2_lr5e-6/roberta-large-physicaliqa-arc1',
     # 'arc1_rs42': 'outputs/roberta-large_rs42_acb4_lr5e-6/roberta-large-physicaliqa-arc1',
     'arc1_rs10061880': 'outputs/roberta-large_rs10061880_acb2_lr5e-6/roberta-large-physicaliqa-arc1',
-    # 'arc2_rs0': 'outputs/roberta-large_rs0_acb1_lr1e-6/roberta-large-physicaliqa-arc2',
+    'arc2_rs0': 'outputs/roberta-large_rs0_acb1_lr1e-6/roberta-large-physicaliqa-arc2',
     # 'arc2_rs42': 'outputs/roberta-large_rs42_acb8_lr5e-6/roberta-large-physicaliqa-arc2',
-    # 'arc2_rs10061880': 'outputs/roberta-large_rs10061880_acb1_lr5e-6/roberta-large-physicaliqa-arc2',
+    'arc2_rs10061880': 'outputs/roberta-large_rs10061880_acb1_lr5e-6/roberta-large-physicaliqa-arc2',
 }
 model_to_predictions = {}
 model_to_confidences = {}
@@ -55,14 +57,18 @@ for id1, id2 in itertools.combinations(model_to_predictions.keys(), 2):
 
 # Run ensemble
 # subset = ['standard_rs0', 'standard_rs10061880']
-predictions_df = (pd.DataFrame.from_dict(model_to_predictions) - 0.5) * 2  # Project to predictions to [-1, 1]
-confidences_df = pd.DataFrame.from_dict(model_to_confidences)
-# confidences_df[confidences_df < 0.2] = 0  # Set low confidence values to 0.
-# confidences_df = confidences_df.eq(confidences_df.where(confidences_df != 0).max(1), axis=0).astype(int)  # Get the most confident
+for subset in powerset(model_to_path.keys()):
+    if len(subset) <= 1: continue
+    subset = list(subset)
+    predictions_df = (pd.DataFrame.from_dict(model_to_predictions) - 0.5) * 2  # Project to predictions to [-1, 1]
+    confidences_df = pd.DataFrame.from_dict(model_to_confidences)
+    # confidences_df[confidences_df < 0.2] = 0  # Set low confidence values to 0.
+    # confidences_df = confidences_df.eq(confidences_df.where(confidences_df != 0).max(1), axis=0).astype(int)  # Get the most confident
 
-scaled_df = predictions_df.mul(confidences_df, fill_value=1)  # Scale the predictions by multiplying with confidence
-print('Predictions', predictions_df)
-print('Confidences', confidences_df)
-print('Scaled', scaled_df)
-final_predictions = scaled_df.mean(axis=1) > 0  # Take the average of each row for ensembled predictions
-print(accuracy_score(labels, final_predictions.values.squeeze().tolist()))
+    scaled_df = predictions_df.mul(confidences_df, fill_value=1)[subset]  # Scale the predictions by multiplying with confidence
+    # print('Predictions', predictions_df)
+    # print('Confidences', confidences_df)
+    # print('Scaled', scaled_df)
+    final_predictions = scaled_df.mean(axis=1) > 0  # Take the average of each row for ensembled predictions
+    print(subset)
+    print(accuracy_score(labels, final_predictions.values.squeeze().tolist()))
