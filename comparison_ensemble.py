2ec2b3cce2c4fd800be406838c276225c3506f4a
Deniz Beser <denizbeser15@gmail.com>
2020-05-19 11:44:21 -0400

    ensembling

--- comparison_ensemble.py
+++ comparison_ensemble.py
@@ -60,7 +60,7 @@ print('\n')
 # Run ensemble
 # subset = ['standard_rs0', 'standard_rs10061880', 'arc1_rs10061880', 'arc2_rs10061880'] # 81.28
 
-print(f'accuracy,{model_to_path.keys()}'.replace(' ','').replace('[','').replace(']',''))
+print(f'accuracy,{list(model_to_path.keys())}'.replace(' ','').replace('[','').replace(']','')) # print for csv
 for subset in powerset(model_to_path.keys()):
     if len(subset) <= 1: continue
     subset = list(subset)
@@ -75,4 +75,5 @@ for subset in powerset(model_to_path.keys()):
     # print('Scaled', scaled_df)
     final_predictions = scaled_df.mean(axis=1) > 0  # Take the average of each row for ensembled predictions
     accuracy = accuracy_score(labels, final_predictions.values.squeeze().tolist())
-    print(f'{accuracy},{[i in subset for i in model_to_path.keys()]}'.replace(' ','').replace('[','').replace(']',''))
+    # print(f'{accuracy},{subset}')
+    print(f'{accuracy},{[int(i in subset) for i in model_to_path.keys()]}'.replace(' ','').replace('[','').replace(']','')) # CSV
