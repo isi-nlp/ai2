70efdb92d69ad1a78599ca6e6ae62370763b6e60
Alex Hedges <ahedges@isi.edu>
2020-06-16 17:41:40 -0400

    Create balanced subsets of CycIC

--- scripts/real_cycic.py
+++ scripts/real_cycic.py
@@ -1,13 +1,16 @@
 import argparse
-from collections import Counter
+from collections import Counter, defaultdict
+import itertools
 import json
 from pathlib import Path
+import random
 
 
 def main() -> None:
     p = argparse.ArgumentParser(description=__doc__)
     p.add_argument('--input-dir', type=Path, default=Path('CycIC-train-dev'))
     p.add_argument('--output-dir', type=Path, default=Path('task_data', 'cycic-real-train-dev'))
+    p.add_argument('--subset', type=float, default=None)
     args = p.parse_args()
 
     for in_split, out_split in (('training', 'train'), ('dev', 'dev')):
@@ -39,6 +42,22 @@ def main() -> None:
                 raise ValueError(f'Invalid "questionType" {new_question["questionType"]}')
             new_questions.append(new_question)
             new_labels.append(label['correct_answer'])
+
+        if out_split == 'train' and args.subset is not None:
+            random.seed(42)
+            merged = list(zip(new_questions, new_labels, itertools.count()))
+            categories = defaultdict(list)
+            for question_label in merged:
+                cats = tuple(question_label[0]['categories'])
+                categories[cats].append(question_label)
+            categories = dict(categories)
+            subset = []
+            for partition in categories.values():
+                cutoff = round(len(partition) * args.subset)
+                randomized = random.sample(partition, k=len(partition))[:cutoff]
+                subset.extend(randomized)
+            subset.sort(key=lambda x: x[2])
+            new_questions, new_labels, _ = zip(*subset)
 
         args.output_dir.mkdir(parents=True, exist_ok=True)
         with (args.output_dir / f'{out_split}.jsonl').open('w') as file:
