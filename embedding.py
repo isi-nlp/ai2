85338983dd751673a3a3beb9e919f389bca37330
David Li <w245li@edu.uwaterloo.ca>
2020-06-24 14:55:15 -0400

    Restructured embedding calculation

--- embedding.py
+++ embedding.py
@@ -6,6 +6,7 @@ import torch
 from loguru import logger
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+import pickle
 
 from model import Classifier
 
@@ -16,33 +17,55 @@ ROOT_PATH = pathlib.Path(__file__).parent.absolute()
 @hydra.main(config_path="config/embed.yaml")
 def embedding(config):
     logger.info(config)
-
-    # Load in the check pointed model from check point file
     device = 'cpu' if not torch.cuda.is_available() else "cuda"
+
+    # Initiate the Distance Evaluation Dictionary to store all information needed to evaluate
+    distance_eval_dict = {'task_name': config['task_name'], 'task_formula': config['formula'],
+                          'train_path': ROOT_PATH / config['train_x'], 'dev_path': ROOT_PATH / config['val_x'],
+                          'checkpoint_names': [], 'train_embed': [], 'dev_embed': []}
+
+    # First we use an out of box model to encode the train and dev files as baseline
     model = Classifier(config)
-    if config['checkpoints_dir'] and config['checkpoint_name']:
-        checkpoint = torch.load(ROOT_PATH / config['checkpoints_dir'] / f"{config['checkpoint_name']}.ckpt",
-                                map_location=device)
-        model.load_state_dict(checkpoint['state_dict'])
     model.to(device)
     model.eval()
+    logger.info('Parsing embeddings using an out of box model')
+    distance_eval_dict['checkpoint_names'].append('out_of_box')
+    distance_eval_dict['train_embed'].append(
+        calculate_embeddings(a_classifier=model, compute_device=device, feature=config['feature'],
+                             text_path=ROOT_PATH / config['train_x'], label_path=ROOT_PATH / config['train_y']))
+    distance_eval_dict['dev_embed'].append(
+        calculate_embeddings(a_classifier=model, compute_device=device, feature=config['feature'],
+                             text_path=ROOT_PATH / config['val_x'], label_path=ROOT_PATH / config['val_y']))
 
-    # Store output lists
-    np.save(f"{config['model']}-{config['task_name']}_train-{config['checkpoint_name']}-{config['feature']}.npy",
-            calculate_embeddings(a_classifier=model,
-                                 text_path=ROOT_PATH / config['train_x'],
-                                 label_path=ROOT_PATH / config['train_y'],
-                                 compute_device=device, feature=config['feature']))
-    np.save(f"{config['model']}-{config['task_name']}_dev-{config['checkpoint_name']}-{config['feature']}.npy",
-            calculate_embeddings(a_classifier=model,
-                                 text_path=ROOT_PATH / config['val_x'],
-                                 label_path=ROOT_PATH / config['val_y'],
-                                 compute_device=device, feature=config['feature']))
+    if config['checkpoint_list']:
+        with open(ROOT_PATH / config['checkpoint_list'], 'r') as checkpoint_list:
+            for a_checkpoint_file_location in checkpoint_list:
+                # Load in the checkpoint file for the mdoel
+                model = Classifier(config)
+                checkpoint = torch.load(ROOT_PATH / a_checkpoint_file_location.strip(), map_location=device)
+                model.load_state_dict(checkpoint['state_dict'])
+                model.to(device)
+                model.eval()
+
+                # Extract the model name and calculate embeddings
+                model_name = a_checkpoint_file_location.strip().split('/')[-1].split('.')[0]
+                logger.info(f'Parsing embeddings using {model_name}')
+
+                distance_eval_dict['checkpoint_names'].append(model_name)
+                distance_eval_dict['train_embed'].append(
+                    calculate_embeddings(a_classifier=model, compute_device=device, feature=config['feature'],
+                                         text_path=ROOT_PATH / config['train_x'], label_path=ROOT_PATH / config['train_y']))
+                distance_eval_dict['dev_embed'].append(
+                    calculate_embeddings(a_classifier=model, compute_device=device, feature=config['feature'],
+                                         text_path=ROOT_PATH / config['val_x'], label_path=ROOT_PATH / config['val_y']))
+
+    # Pickle dump the dictionary for embedding distance calculation
+    with open(f"{config['model']}-{config['task_name']}-{config['feature']}.pickle", 'w+') as output_file:
+        pickle.dump(distance_eval_dict, output_file)
 
 
 # Helper function for parsing embeddings and return a 2D numpy array
 def calculate_embeddings(a_classifier: Classifier, text_path: str, label_path: str, compute_device: str, feature: str):
-
     embedding_list = []
 
     # Forward propagate the model to get a list of predictions and their respective confidence
