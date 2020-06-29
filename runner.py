08682b4cd4d30fbdec682cb0735458ba0f8e8a4c
Deniz Beser <denizbeser15@gmail.com>
2020-06-29 11:42:42 -0400

    alph rerun

--- runner.py
+++ runner.py
@@ -6,10 +6,10 @@ parameter_options = {
                 # 'train_data_slice': ['25', '90'],
                 'train_data_slice': ['100'],
                 # 'task2': ['cn_10k', 'cn_20k', 'cn_40k', 'cn_physical_10k'],
-                'task2': ['','cn_10k'],
-                # 'task2': [''],
+                # 'task2': ['','cn_10k'],
+                'task2': [''],
                 # 'architecture': ['standard', 'include_answers_in_context', 'embed_all_sep_mean'],
-                'architecture': ['embed_all_sep_mean'],
+                'architecture': ['standard'],
                 # 'random_seed': ['0', '42', '10061880'],
                 'random_seed': ['0'],
               }
