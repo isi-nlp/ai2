49998a6f7b404b127a4d82ec3f5a9d2f6d21476a
Deniz Beser <denizbeser15@gmail.com>
2020-06-30 17:38:38 -0400

    docker for all

--- runner.py
+++ runner.py
@@ -2,16 +2,16 @@ import os
 
 parameter_options = {
                 # 'task': ['alphanli', 'hellaswag', 'physicaliqa', 'socialiqa'],
-                'task': ['alphanli'],
+                'task': ['physicaliqa'],
                 # 'train_data_slice': ['25', '90'],
                 'train_data_slice': ['100'],
                 # 'task2': ['cn_10k', 'cn_20k', 'cn_40k', 'cn_physical_10k'],
                 # 'task2': ['','cn_10k'],
                 'task2': [''],
                 # 'architecture': ['standard', 'include_answers_in_context', 'embed_all_sep_mean'],
-                'architecture': ['standard'],
+                'architecture': ['include_answers_in_context'],
                 # 'random_seed': ['0', '42', '10061880'],
-                'random_seed': ['0'],
+                'random_seed': ['10061880'],
               }
 
 # Create all possible combinations of parameters
