7c6bf4f57669ee70eb86e5e4bc26ca66f7df54ba
Deniz Beser <denizbeser15@gmail.com>
2020-06-30 13:08:07 -0400

    piqa docker files

--- filter_submission_model.py
+++ filter_submission_model.py
@@ -1,19 +1,30 @@
 import os
 
 models = f"""
-'alphanli_100_include_answers_in_context_0', 'alphanli_100_cn_10k_standard_0', 'alphanli_100_cn_10k_include_answers_in_context_0', 'alphanli_100_standard_42'
+'alphanli_100_include_answers_in_context_0', 
+'alphanli_100_cn_10k_standard_0', 
+'alphanli_100_cn_10k_include_answers_in_context_0', 
+'alphanli_100_standard_42'
 physicaliqa_100_cn_10k_standard_42
 physicaliqa_100_include_answers_in_context_10061880
 physicaliqa_100_cn_10k_include_answers_in_context_0
 physicaliqa_100_standard_42
 'hellaswag_100_standard_0',
-'hellaswag_100_include_answers_in_context_42', 'hellaswag_100_cn_10k_standard_0', 'hellaswag_100_cn_10k_include_answers_in_context_0', 'hellaswag_100_cn_10k_embed_all_sep_mean_0', 'hellaswag_100_embed_all_sep_mean_42'
-'socialiqa_100_cn_10k_standard_42', 'socialiqa_100_cn_10k_include_answers_in_context_0', 'socialiqa_100_cn_10k_embed_all_sep_mean_10061880', 'socialiqa_100_include_answers_in_context_0', 'socialiqa_100_embed_all_sep_mean_42',
+'hellaswag_100_include_answers_in_context_42', 
+'hellaswag_100_cn_10k_standard_0', 
+'hellaswag_100_cn_10k_include_answers_in_context_0',
+'hellaswag_100_cn_10k_embed_all_sep_mean_0', 
+'hellaswag_100_embed_all_sep_mean_42'
+'socialiqa_100_cn_10k_standard_42', 
+'socialiqa_100_cn_10k_include_answers_in_context_0', 
+'socialiqa_100_cn_10k_embed_all_sep_mean_10061880',
+'socialiqa_100_include_answers_in_context_0', 
+'socialiqa_100_embed_all_sep_mean_42',
 'socialiqa_100_standard_42
 """
 
 for model in models.split():
-    m = models.replace('\'','').replace(',','')
+    m = models.replace('\'','').replace(',','').replace(' ','')
     task = m.split('_')[0]
     print(m)
     for i in range(4):
