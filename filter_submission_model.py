2cf03bb250bc3a20fdb5d1ee0e4797425b2d565f
Deniz Beser <denizbeser15@gmail.com>
2020-06-30 18:10:23 -0400

    docker for all

--- filter_submission_model.py
+++ filter_submission_model.py
@@ -5,10 +5,10 @@ models = f"""
 'alphanli_100_cn_10k_standard_0', 
 'alphanli_100_cn_10k_include_answers_in_context_0', 
 'alphanli_100_standard_42'
-physicaliqa_100_cn_10k_standard_42
-physicaliqa_100_include_answers_in_context_10061880
-physicaliqa_100_cn_10k_include_answers_in_context_0
-physicaliqa_100_standard_42
+ 'physicaliqa_100_cn_10k_standard_42
+'physicaliqa_100_include_answers_in_context_10061880
+'physicaliqa_100_cn_10k_include_answers_in_context_0
+'physicaliqa_100_standard_42'
 'hellaswag_100_standard_0',
 'hellaswag_100_include_answers_in_context_42', 
 'hellaswag_100_cn_10k_standard_0', 
@@ -20,7 +20,7 @@ physicaliqa_100_standard_42
 'socialiqa_100_cn_10k_embed_all_sep_mean_10061880',
 'socialiqa_100_include_answers_in_context_0', 
 'socialiqa_100_embed_all_sep_mean_42',
-'socialiqa_100_standard_42
+'socialiqa_100_standard_42'
 """
 
 for model in models.split():
@@ -28,8 +28,7 @@ for model in models.split():
     task = m.split('_')[0]
     print(m)
     os.system(f'ls outputs/{m}/checkpoints/')
-    # for i in range(4):
-    #     try:
-    #         os.system(f'cp outputs/{m}/checkpoints/_ckpt_epoch_{i}.ckpt {task}_submission_models/{m}.ckpt')
-    #     except:
-    #         pass
+    try:
+        os.system(f'cp outputs/{m}/checkpoints/_ckpt_epoch_3.ckpt {task}_submission_models/{m}.ckpt')
+    except:
+        pass
--- runner.py
+++ runner.py
@@ -1,32 +1,71 @@
 import os
 
-parameter_options = {
-                'task': ['alphanli', 'hellaswag', 'physicaliqa', 'socialiqa'],
-                # 'task': ['physicaliqa'],
-                # 'train_data_slice': ['25', '90'],
-                'train_data_slice': ['100'],
-                # 'task2': ['cn_10k', 'cn_20k', 'cn_40k', 'cn_physical_10k'],
-                'task2': ['','cn_10k'],
-                'task2': [''],
-                'architecture': ['standard', 'include_answers_in_context', 'embed_all_sep_mean'],
-                # 'architecture': ['include_answers_in_context'],
-                'random_seed': ['0', '42', '10061880'],
-                # 'random_seed': ['10061880'],
-              }
+# parameter_options = {
+#                 'task': ['alphanli', 'hellaswag', 'physicaliqa', 'socialiqa'],
+#                 # 'task': ['physicaliqa'],
+#                 # 'train_data_slice': ['25', '90'],
+#                 'train_data_slice': ['100'],
+#                 # 'task2': ['cn_10k', 'cn_20k', 'cn_40k', 'cn_physical_10k'],
+#                 'task2': ['','cn_10k'],
+#                 'task2': [''],
+#                 'architecture': ['standard', 'include_answers_in_context', 'embed_all_sep_mean'],
+#                 # 'architecture': ['include_answers_in_context'],
+#                 'random_seed': ['0', '42', '10061880'],
+#                 # 'random_seed': ['10061880'],
+#               }
+#
+# # Create all possible combinations of parameters
+# parameter_combinations = [[]]
+# for parameter_name, options in parameter_options.items():
+#     new_combinations = []
+#     for combination in parameter_combinations:
+#         for option in options:
+#             new_combination = combination + [(parameter_name,option)]
+#             new_combinations.append(new_combination)
+#     parameter_combinations = new_combinations
+#
+# for i, combination in enumerate(parameter_combinations):
+#     # experiment_id = '_'.join(option for _, option in combination if option != '')
+#     break
 
-# Create all possible combinations of parameters
-parameter_combinations = [[]]
-for parameter_name, options in parameter_options.items():
-    new_combinations = []
-    for combination in parameter_combinations:
-        for option in options:
-            new_combination = combination + [(parameter_name,option)]
-            new_combinations.append(new_combination)
-    parameter_combinations = new_combinations
 
-for i, combination in enumerate(parameter_combinations):
-    experiment_id = '_'.join(option for _, option in combination if option != '')
-    # experiment_id = 'newsplit_'+experiment_id
+models = f"""
+'alphanli_100_include_answers_in_context_0', 
+'alphanli_100_cn_10k_standard_0', 
+'alphanli_100_cn_10k_include_answers_in_context_0', 
+'alphanli_100_standard_42'
+'physicaliqa_100_cn_10k_standard_42
+'physicaliqa_100_include_answers_in_context_10061880
+'physicaliqa_100_cn_10k_include_answers_in_context_0
+'physicaliqa_100_standard_42'
+'hellaswag_100_standard_0',
+'hellaswag_100_include_answers_in_context_42', 
+'hellaswag_100_cn_10k_standard_0', 
+'hellaswag_100_cn_10k_include_answers_in_context_0',
+'hellaswag_100_cn_10k_embed_all_sep_mean_0', 
+'hellaswag_100_embed_all_sep_mean_42'
+'socialiqa_100_cn_10k_standard_42', 
+'socialiqa_100_cn_10k_include_answers_in_context_0', 
+'socialiqa_100_cn_10k_embed_all_sep_mean_10061880',
+'socialiqa_100_include_answers_in_context_0', 
+'socialiqa_100_embed_all_sep_mean_42',
+'socialiqa_100_standard_42'
+"""
+
+for model in models.split():
+    experiment_id = model.replace('\'', '').replace(',', '').replace(' ', '')
+    combination = [('train_data_slice','100')]
+    combination.append(('random_seed', experiment_id.split('_')[-1]))
+    combination.append(('task', experiment_id.split('_')[0]))
+    if 'cn_10k' in experiment_id:
+        combination.append(('task2','cn_10k'))
+    if 'standard' in experiment_id:
+        combination.append(('architecture','standard'))
+    elif 'include_answers_in_context' in experiment_id:
+        combination.append(('architecture','include_answers_in_context'))
+    elif 'embed_all_sep_mean' in experiment_id:
+        combination.append(('architecture','embed_all_sep_mean'))
+
 
     os.system(f"sbatch "
           # Additional SLURM specifications
