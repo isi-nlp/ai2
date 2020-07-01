bbb9b9462ce31222b5d37a49f534499309ec1d0d
Deniz Beser <denizbeser15@gmail.com>
2020-07-01 10:36:14 -0400

    docker for all

--- filter_submission_model.py
+++ filter_submission_model.py
@@ -1,26 +1,26 @@
 import os
 
+# 'alphanli_100_include_answers_in_context_0',
+# 'alphanli_100_cn_10k_standard_0',
+# 'alphanli_100_cn_10k_include_answers_in_context_0',
+# 'alphanli_100_standard_42'
+# 'physicaliqa_100_cn_10k_standard_42
+# 'physicaliqa_100_include_answers_in_context_10061880
+# 'physicaliqa_100_cn_10k_include_answers_in_context_0
+# 'physicaliqa_100_standard_42'
+# 'socialiqa_100_cn_10k_standard_42',
+# 'socialiqa_100_cn_10k_include_answers_in_context_0',
+# 'socialiqa_100_cn_10k_embed_all_sep_mean_10061880',
+# 'socialiqa_100_include_answers_in_context_0',
+# 'socialiqa_100_embed_all_sep_mean_42',
+# 'socialiqa_100_standard_42'
 models = f"""
-'alphanli_100_include_answers_in_context_0', 
-'alphanli_100_cn_10k_standard_0', 
-'alphanli_100_cn_10k_include_answers_in_context_0', 
-'alphanli_100_standard_42'
- 'physicaliqa_100_cn_10k_standard_42
-'physicaliqa_100_include_answers_in_context_10061880
-'physicaliqa_100_cn_10k_include_answers_in_context_0
-'physicaliqa_100_standard_42'
 'hellaswag_100_standard_0',
 'hellaswag_100_include_answers_in_context_42', 
 'hellaswag_100_cn_10k_standard_0', 
 'hellaswag_100_cn_10k_include_answers_in_context_0',
 'hellaswag_100_cn_10k_embed_all_sep_mean_0', 
 'hellaswag_100_embed_all_sep_mean_42'
-'socialiqa_100_cn_10k_standard_42', 
-'socialiqa_100_cn_10k_include_answers_in_context_0', 
-'socialiqa_100_cn_10k_embed_all_sep_mean_10061880',
-'socialiqa_100_include_answers_in_context_0', 
-'socialiqa_100_embed_all_sep_mean_42',
-'socialiqa_100_standard_42'
 """
 
 for model in models.split():
--- runner.py
+++ runner.py
@@ -32,51 +32,51 @@ for i, combination in enumerate(parameter_combinations):
 # # 'alphanli_100_cn_10k_standard_0',
 # # 'alphanli_100_cn_10k_include_answers_in_context_0',
 # # 'alphanli_100_standard_42'
-# # """
-# models = f"""
 # 'physicaliqa_100_cn_10k_standard_42
 # 'physicaliqa_100_include_answers_in_context_10061880
 # 'physicaliqa_100_cn_10k_include_answers_in_context_0
 # 'physicaliqa_100_standard_42'
-# 'hellaswag_100_standard_0',
-# 'hellaswag_100_include_answers_in_context_42',
-# 'hellaswag_100_cn_10k_standard_0',
-# 'hellaswag_100_cn_10k_include_answers_in_context_0',
-# 'hellaswag_100_cn_10k_embed_all_sep_mean_0',
-# 'hellaswag_100_embed_all_sep_mean_42'
 # 'socialiqa_100_cn_10k_standard_42',
 # 'socialiqa_100_cn_10k_include_answers_in_context_0',
 # 'socialiqa_100_cn_10k_embed_all_sep_mean_10061880',
 # 'socialiqa_100_include_answers_in_context_0',
 # 'socialiqa_100_embed_all_sep_mean_42',
 # 'socialiqa_100_standard_42'
+# # """
+models = f"""
+# 'hellaswag_100_standard_0',
+# 'hellaswag_100_include_answers_in_context_42',
+# 'hellaswag_100_cn_10k_standard_0',
+# 'hellaswag_100_cn_10k_include_answers_in_context_0',
+# 'hellaswag_100_cn_10k_embed_all_sep_mean_0',
+# 'hellaswag_100_embed_all_sep_mean_42'
 # """
 #
-# for model in models.split():
-#     print(model)
-#     experiment_id = model.replace('\'', '').replace(',', '').replace(' ', '')
-#     combination = [('train_data_slice','100')]
-#     combination.append(('random_seed', experiment_id.split('_')[-1]))
-#     combination.append(('task', experiment_id.split('_')[0]))
-#     if 'cn_10k' in experiment_id:
-#         combination.append(('task2','cn_10k'))
-#     if 'standard' in experiment_id:
-#         combination.append(('architecture','standard'))
-#     elif 'include_answers_in_context' in experiment_id:
-#         combination.append(('architecture','include_answers_in_context'))
-#     elif 'embed_all_sep_mean' in experiment_id:
-#         combination.append(('architecture','embed_all_sep_mean'))
-#     print(combination)
+for model in models.split():
+    experiment_id = model.replace('\'', '').replace(',', '').replace(' ', '')
+    print(experiment_id)
+    combination = [('train_data_slice','100')]
+    combination.append(('random_seed', experiment_id.split('_')[-1]))
+    combination.append(('task', experiment_id.split('_')[0]))
+    if 'cn_10k' in experiment_id:
+        combination.append(('task2','cn_10k'))
+    if 'standard' in experiment_id:
+        combination.append(('architecture','standard'))
+    elif 'include_answers_in_context' in experiment_id:
+        combination.append(('architecture','include_answers_in_context'))
+    elif 'embed_all_sep_mean' in experiment_id:
+        combination.append(('architecture','embed_all_sep_mean'))
+    print(combination)
 
     os.system(f"sbatch "
           # Additional SLURM specifications
           f"-J {experiment_id} "
           f"-o outputs/slurm/{experiment_id}.out "
           # Ephemeral specifications - sudo sacctmgr modify user beser set MaxJobs=25
-          f"--partition=ephemeral "
-          f"--qos=ephemeral "
-          f"--time=12:00:00 "
-          f"{'--gpus-per-task=2 ' if 'hellaswag' in experiment_id else ''}"
+          # f"--partition=ephemeral "
+          # f"--qos=ephemeral "
+          # f"--time=12:00:00 "
+          # f"{'--gpus-per-task=2 ' if 'hellaswag' in experiment_id else ''}"
           f"slurm/run_saga.sh "
           # Python script commands
           f"\""
