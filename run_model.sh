9d3ef877977c84545c78914a39758b6cf8aca10b
Alex Hedges <ahedges@isi.edu>
2020-07-02 13:28:27 -0400

    Prepare Dockerfiles for new models

--- run_model.sh
+++ run_model.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 INPUT_PATH=/data/cycic.jsonl
 TEMP_PATH=temp.jsonl
 OUTPUT_PATH=/results
-MODEL_PATH=cycic_2020_06_17_s42.ckpt
+MODEL_PATH=cycic.ckpt
 
 python sub_cycic.py --input-file $INPUT_PATH --output-file $TEMP_PATH
 python eval.py task=cycic_real with_true_label=False checkpoint_path=$MODEL_PATH \
