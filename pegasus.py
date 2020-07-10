aa046dc867ac1d0ec6ec2a8042543bab50ebfc30
Joseph Cecil <cecil@ads.isi.edu>
2020-07-10 10:47:43 -0400

    Fix ensembling parameters setup

--- pegasus.py
+++ pegasus.py
@@ -93,18 +93,23 @@ def main(params: Parameters):
 
     ensemble_params = params.namespace('ensemble')
     for task, jobs_info in task_to_jobs_info.items():
+        models_list = []
         for job_info in jobs_info:
             predictions_path = directory_for(job_info['predictions'].locator) / job_info['predictions'].value
             confidence_path = directory_for(job_info['confidence'].locator) / job_info['confidence'].value
-            ensemble_params = ensemble_params.unify({
-                'models': {
-                    'task': job_info['task'],
-                    'train_data_slice': job_info['train_data_slice'],
-                    'parameter_combination': job_info['parameter_combination'],
-                    'predictions': predictions_path,
-                    'confidence': confidence_path,
-                }
+
+            models_list.append({
+                'task': job_info['task'],
+                'train_data_slice': job_info['train_data_slice'],
+                'parameter_combination': job_info['parameter_combination'],
+                'predictions': predictions_path,
+                'confidence': confidence_path,
             })
+
+        ensemble_params = ensemble_params.unify({
+            'models': {task: models_list}
+        })
+
     run_python_on_parameters(
         Locator(('ensembled',)),
         'ensemble',
