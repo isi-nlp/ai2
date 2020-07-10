f6a26daf1fe5a97467b2d83aa64ea3d2157a53a6
Joseph Cecil <cecil@ads.isi.edu>
2020-07-10 10:37:01 -0400

    Fix another typo

--- pegasus.py
+++ pegasus.py
@@ -99,14 +99,14 @@ def main(params: Parameters):
     for task, jobs_info in task_to_jobs_info.items():
         for job_info in jobs_info:
             predictions_path = directory_for(job_info['predictions'].locator) / job_info['predictions'].value
-            confidences_path = directory_for(job_info['confidences'].locator) / job_info['confidences'].value
+            confidence_path = directory_for(job_info['confidence'].locator) / job_info['confidence'].value
             ensemble_params = ensemble_params.unify({
                 'models': {
                     'task': job_info['task'],
                     'train_data_slice': job_info['train_data_slice'],
                     'parameter_combination': job_info['parameter_combination'],
                     'predictions': predictions_path,
-                    'confidences': confidences_path,
+                    'confidence': confidence_path,
                 }
             })
     run_python_on_parameters(
@@ -114,7 +114,7 @@ def main(params: Parameters):
         'ensemble',
         ensemble_params,
         depends_on=flatten([
-            (job_info['predictions'], job_info['confidences'])
+            (job_info['predictions'], job_info['confidence'])
             for jobs_info in task_to_jobs_info.values()
             for job_info in jobs_info
         ])
