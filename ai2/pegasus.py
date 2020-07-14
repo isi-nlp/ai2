dd72e9067be828e68e4778fa0dea992aab89acaa
Joseph Cecil <jcecil@isi.edu>
2020-07-14 15:59:52 -0400

    Fix typo

--- ai2/pegasus.py
+++ ai2/pegasus.py
@@ -95,7 +95,7 @@ def main(params: Parameters):
 
     ensemble_locator = Locator(('ensembled',))
     ensemble_params = params.namespace('ensemble')
-    ensemble_params.unify({
+    ensemble_params = ensemble_params.unify({
         'data_sizes': params.arbitrary_list('parameter_options.train_data_slice'),
         'output_file': directory_for(ensemble_locator) / ensemble_params.creatable_file('output_file_name'),
     })
