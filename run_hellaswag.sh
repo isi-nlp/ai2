44ba783af1d5a1cd52efd445d2a0b549d70d418f
Deniz Beser <denizbeser15@gmail.com>
2020-07-20 16:43:02 -0400

    remove ai2 docker

--- run_hellaswag.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-python predict.py --input-file /data/hellaswag.jsonl --output-file /results/predictions.lst
--- run_physicaliqa.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-python predict.py --input-file /data/physicaliqa.jsonl --output-file /results/predictions.lst
