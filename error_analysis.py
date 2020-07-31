a2d4b8ad863c6d1c8d2fb966fed19737f9eb56fe
Deniz Beser <denizbeser15@gmail.com>
2020-07-31 17:35:10 -0400

    error analysis

--- error_analysis.py
+++ error_analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import ast
 
 for task in ['physicaliqa', 'socialiqa']:
     gold_labels = pd.read_csv(f"task_data/{task}-train-dev/internal-dev-labels.lst", sep='\t', header=None).values.squeeze().tolist()
@@ -11,7 +12,14 @@ for task in ['physicaliqa', 'socialiqa']:
     results = []
     for i, gold in enumerate(gold_labels):
         if gold == preds_90[i] and gold != preds_25[i]:
-            results.append((preds_90[i], preds_25[i], data[i]))
+            qa_dict = ast.literal_eval(data[i])
+            if task == 'physicaliqa':
+                results.append((preds_90[i], preds_25[i], qa_dict["goal"], qa_dict['sol1'], qa_dict['sol2']))
+            else:
+                results.append((preds_90[i], preds_25[i], qa_dict["context"], qa_dict["question"], qa_dict['answerA'], qa_dict['answerB'], qa_dict['answerC']))
 
-    df = pd.DataFrame(results, columns =['90', '25', 'QA'])
+    if task == 'physicaliqa':
+        df = pd.DataFrame(results, columns =['90', '25', 'Question', 'Answer 1', 'Answer 2'])
+    else:
+        df = pd.DataFrame(results, columns =['90', '25', 'Question', 'Context', 'Answer 1', 'Answer 2', 'Answer 3'])
     df.to_csv(f'{task}_errors_90vs25.csv', na_rep='-')
