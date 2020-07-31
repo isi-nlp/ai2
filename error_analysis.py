8738f1fe2f794c94e9a4d52fbdb58686499d926c
Deniz Beser <denizbeser15@gmail.com>
2020-07-31 17:24:41 -0400

    error analysis

--- error_analysis.py
+++ error_analysis.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 for task in ['physicaliqa', 'socialiqa']:
     gold_labels = pd.read_csv(f"task_data/{task}-train-dev/internal-dev-labels.lst", sep='\t', header=None).values.squeeze().tolist()
-    data = pd.read_csv(f"task_data/{task}-train-dev/internal-dev.jsonl", header=None).values.squeeze().tolist()
+    with open(f"task_data/{task}-train-dev/internal-dev.jsonl", 'r') as dataf:
+        data = dataf.readlines()
 
     preds_90 = pd.read_csv(f'outputs/{task}_90_standard_42/predictions.lst', sep='\t', header=None).values.squeeze().tolist()
     preds_25 = pd.read_csv(f'outputs/{task}_25_standard_42/predictions.lst', sep='\t', header=None).values.squeeze().tolist()
