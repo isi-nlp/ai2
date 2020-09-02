079e5fa8a4b461b9a0a51b817f76a15d6403cfc0
Alex Hedges <ahedges@isi.edu>
2020-09-01 21:33:28 -0700

    Commit changes from SAGA
    I am deleting my personal copy, and these changes were not committed.
    It's not worth my time to go through them, so I'm just pushing them to
    GitHub.

--- slurm/train_ai2.sh
+++ slurm/train_ai2.sh
@@ -2,14 +2,15 @@
 
 #SBATCH --account=mics
 #SBATCH --partition=mics
+#SBATCH --nodelist=saga26
 #SBATCH --ntasks=1    # Number of instances launched of this job.
-#SBATCH --time=24:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem-per-cpu=4G
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-task=rtx2080ti:1
 #SBATCH --job-name=TRAIN_AI2
 #SBATCH --output=outputs/slurm/%x-%j.out    # %x-%j means JOB_NAME-JOB_ID.
-#SBATCH --array=0-3   # Submitting an array of (n-m+1) jobs, with $SLURM_ARRAY_TASK_ID ranging from n to m.
+#SBATCH --array=0-0   # Submitting an array of (n-m+1) jobs, with $SLURM_ARRAY_TASK_ID ranging from n to m.
 
 set -euo pipefail
 
@@ -25,10 +26,10 @@ echo "Job Array ID / Job ID: $SLURM_ARRAY_JOB_ID / $SLURM_JOB_ID"
 echo "This is job $((SLURM_ARRAY_TASK_ID + 1)) out of $SLURM_ARRAY_TASK_COUNT jobs."
 
 # Create a total array of models and tasks and permute them
-allTask=(alphanli hellaswag physicaliqa socialiqa)
+allTask=(physicaliqa)
 task=${allTask[${SLURM_ARRAY_TASK_ID}]}
 echo
-time python -u train.py task="$task" hydra.run.dir="outputs/$SLURM_JOB_ID"
+time python -u train.py task="$task" hydra.run.dir="outputs/$SLURM_JOB_ID" #build_on_pretrained_model="pretrained_race.ckpt"
 echo
 
 # Finishing up the job and copy the output off of staging
