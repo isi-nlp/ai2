20ff86b7cf29b8f47eec2191f1b26afa06a7cab4
Joe Cecil <jcecil@isi.edu>
2021-05-05 11:50:41 -0400

    Code to split data before passing it to training script

--- ai2/compare_models.py
+++ ai2/compare_models.py
@@ -1,7 +1,8 @@
 """
 Train two slightly different RoBERTa models and compare them on
 """
-from typing import List, Tuple, Any
+from pathlib import Path
+from typing import Any, Dict, List, Tuple
 
 from more_itertools import only
 
@@ -18,6 +19,7 @@ from pegasus_wrapper.resource_request import ResourceRequest
 from pegasus_wrapper.locator import Locator
 from pegasus_wrapper.artifact import ValueArtifact
 
+import ai2.random_slice as random_slice_script
 import ai2.train as train_script
 import ai2.percent_agreement as percent_agreement_script
 from ai2.pegasus import override_generality, override_matches
@@ -33,15 +35,101 @@ DEFAULT_MAX_JOBS_ON_MICS = 2
 ParameterCombination = List[Tuple[str, Any]]
 
 
-def main(params: Parameters):
+def run_random_slice(
+        slice_locator: Locator,
+        *,
+        input: ValueArtifact,
+        output: Path,
+        random_seed: int,
+        fraction: float,
+) -> ValueArtifact:
+    slice_job = run_python_on_parameters(
+        slice_locator,
+        random_slice_script,
+        Parameters.from_mapping({
+            "input": input.value,
+            "output": output,
+            "random_seed": random_seed,
+            "fraction": fraction,
+        }),
+        depends_on=[input],
+    )
+    return ValueArtifact(
+        value=output,
+        depends_on=immutableset([slice_job])
+    )
+
+
+def run_random_slice_for_x_y_pair(
+        slice_locator: Locator,
+        *,
+        input_x: ValueArtifact,
+        input_y: ValueArtifact,
+        output_x: Path,
+        output_y: Path,
+        random_seed: int,
+        fraction: float,
+) -> Tuple[ValueArtifact, ValueArtifact]:
+    sliced_x_artifact = run_random_slice(
+        slice_locator / "x",
+        input=input_x,
+        output=output_x,
+        random_seed=random_seed,
+        fraction=fraction,
+    )
+    sliced_y_artifact = run_random_slice(
+        slice_locator / "y",
+        input=input_y,
+        output=output_y,
+        random_seed=random_seed,
+        fraction=fraction,
+        )
+    return sliced_x_artifact, sliced_y_artifact
+
+
+def compare_models_entrypoint(params: Parameters):
     initialize_vista_pegasus_wrapper(params)
 
     experiment_root = params.creatable_directory('experiment_root')
     project_root = params.existing_directory('project_root')
     params_root = project_root / 'parameters'
+    slice_options = params.arbitrary_list('slice_options')
     parameter_options = params.namespace('parameter_options').as_nested_dicts()
+    all_tasks = immutableset(parameter_options["task"])
 
     max_jobs_on_mics = params.integer('max_jobs_on_mics', default=DEFAULT_MAX_JOBS_ON_MICS)
+
+    # Set up jobs to slice the datasets appropriately
+    task_to_parameters = {
+        task: YAMLParametersLoader().load(params_root / "task" / f"{task}.params")
+        for task in iter(all_tasks)
+    }
+
+    def name_slice(task: str, slice_option: Dict[str, Any]) -> Tuple[str, ...]:
+        return "slice", task, f"{slice_option['seed']}_{slice_option['percent']}"
+
+    slices = {
+        task: [
+            (
+                name_slice(task, slice_option),
+                run_random_slice_for_x_y_pair(
+                    Locator(name_slice(task, slice_option)),
+                    input_x=ValueArtifact.preexisting(
+                        task_to_parameters[task].existing_file("train_x")
+                    ),
+                    input_y=ValueArtifact.preexisting(
+                        task_to_parameters[task].existing_file("train_y")
+                    ),
+                    output_x=experiment_root / "slices" / f"seed{slice_option['seed']}_pct{slice_option['percent']}" / "train.jsonl",
+                    output_y=experiment_root / "slices" / f"seed{slice_option['seed']}_pct{slice_option['percent']}" / "train-labels.lst",
+                    random_seed=slice_option["seed"],
+                    fraction=slice_option["percent"] / 100.,
+                )
+            )
+            for slice_option in slice_options
+        ]
+        for task in iter(all_tasks)
+    }
 
     # Compute all possible combinations of the parameters
     parameter_combinations: List[ParameterCombination] = [[]]
@@ -68,75 +156,80 @@ def main(params: Parameters):
     prediction_artifacts = []
     for idx, combination in enumerate(parameter_combinations):
         task: str = only(option for parameter, option in combination if parameter == 'task')
-        options: Tuple[str] = tuple(str(option) if option != '' else '_default' for _, option in combination)
-        train_locator = model_outputs_locator / Locator(options)
+        options: Tuple[str, ...] = tuple(str(option) if option != '' else '_default' for _, option in combination)
+        for tuple_slice_name, (slice_x_artifact, slice_y_artifact) in slices[task]:
+            string_slice_name = "_".join(tuple_slice_name)
+            train_locator = model_outputs_locator / Locator(options) / string_slice_name
 
-        # Set up common job parameters
-        train_job_params = Parameters.from_key_value_pairs([
-            ('model', params.namespace('model'))
-        ]).unify(
-            params.namespace("train")
-        )
+            # Set up common job parameters
+            train_job_params = Parameters.from_key_value_pairs([
+                ('model', params.namespace('model'))
+            ]).unify(
+                params.namespace("train")
+            )
 
-        # Read in combination-specific parameters
-        train_job_params = train_job_params.unify(Parameters.from_key_value_pairs(combination, namespace_separator=None))
-        for parameter, option in combination:
-            if option != '':
-                parameter_directory = params_root / parameter
-                if parameter_directory.exists():
-                    option_params: Parameters = YAMLParametersLoader().load(
-                        parameter_directory / f'{option}.params'
+            # Read in combination-specific parameters
+            train_job_params = train_job_params.unify(Parameters.from_key_value_pairs(combination, namespace_separator=None))
+            for parameter, option in combination:
+                if option != '':
+                    parameter_directory = params_root / parameter
+                    if parameter_directory.exists():
+                        option_params: Parameters = YAMLParametersLoader().load(
+                            parameter_directory / f'{option}.params'
+                        )
+                        train_job_params = train_job_params.unify(option_params)
+
+            # Because the job parameters tend to indirectly include root.params, which includes a
+            # default partition, we need to override the partition setting to reflect our input
+            # parameters.
+            train_job_params = train_job_params.unify({'partition': params.string('partition')})
+
+            # Process overrides
+            for override in training_overrides:
+                if override_matches(override, dict(combination)):
+                    train_job_params = train_job_params.unify({
+                        parameter_option: value for parameter_option, value in override.items()
+                        if parameter_option != 'parameter_options'
+                    })
+
+            # Messy parameters input. This shouldn't matter to ResourceRequest, though. Maybe clean up
+            # later.
+            resource_request = ResourceRequest.from_parameters(
+                params.unify(train_job_params)
+            )
+
+            # Set common parameters and schedule the job.
+            options_name = "_".join(
+                "=".join(str(x) for x in option_pair)
+                for option_pair in combination
+            )
+            save_path = experiment_root / f"{options_name}_{tuple_slice_name}"
+            train_job_params = train_job_params.unify({
+                'train_x': slice_x_artifact.value,
+                'train_y': slice_y_artifact.value,
+                'save_path': save_path,
+                'save_best_only': False,
+                'save_by_date_and_parameters': False,
+                'eval_after_training': True,
+            })
+            train_job = run_python_on_parameters(
+                train_locator,
+                train_script,
+                train_job_params,
+                depends_on=[slice_x_artifact, slice_y_artifact],
+                resource_request=resource_request,
+            )
+
+            prediction_artifacts.append(
+                (
+                    combination + [("slice", string_slice_name)],
+                    task,
+                    ValueArtifact(
+                        value=save_path / "predictions.lst",
+                        depends_on=immutableset([train_job]),
                     )
-                    train_job_params = train_job_params.unify(option_params)
-
-        # Because the job parameters tend to indirectly include root.params, which includes a
-        # default partition, we need to override the partition setting to reflect our input
-        # parameters.
-        train_job_params = train_job_params.unify({'partition': params.string('partition')})
-
-        # Process overrides
-        for override in training_overrides:
-            if override_matches(override, dict(combination)):
-                train_job_params = train_job_params.unify({
-                    parameter_option: value for parameter_option, value in override.items()
-                    if parameter_option != 'parameter_options'
-                })
-
-        # Messy parameters input. This shouldn't matter to ResourceRequest, though. Maybe clean up
-        # later.
-        resource_request = ResourceRequest.from_parameters(
-            params.unify(train_job_params)
-        )
-
-        # Set common parameters and schedule the job.
-        save_path = experiment_root / "_".join(
-            "=".join(str(x) for x in option_pair)
-            for option_pair in combination
-        )
-        train_job_params = train_job_params.unify({
-            'save_path': save_path,
-            'save_best_only': False,
-            'save_by_date_and_parameters': False,
-            'eval_after_training': True,
-        })
-        train_job = run_python_on_parameters(
-            train_locator,
-            train_script,
-            train_job_params,
-            depends_on=[],
-            resource_request=resource_request,
-        )
-
-        prediction_artifacts.append(
-            (
-                combination,
-                task,
-                ValueArtifact(
-                    value=save_path / "predictions.lst",
-                    depends_on=immutableset([train_job]),
                 )
             )
-        )
 
     # Calculate the percent agreement for all same-task model pairs
     base_percent_agreement_locator = Locator(("percent_agreement",))
@@ -150,9 +243,10 @@ def main(params: Parameters):
             if task1 != task2:
                 continue
 
-            task_parameters: Parameters = YAMLParametersLoader().load(
-                params_root / 'task' / f'{task1}.params'
-            )
+            # task_parameters: Parameters = YAMLParametersLoader().load(
+            #     params_root / 'task' / f'{task1}.params'
+            # )
+            task_parameters: Parameters = task_to_parameters[task1]
             percent_agreement_parameters = params.unify(task_parameters)
 
             model2_name = '__'.join(
@@ -182,4 +276,4 @@ def main(params: Parameters):
 
 
 if __name__ == '__main__':
-    parameters_only_entry_point(main)
+    parameters_only_entry_point(compare_models_entrypoint)
---
+++ ai2/random_slice.py
@@ -0,0 +1,66 @@
+"""
+A script for slicing a dataset using a given seed and fraction.
+"""
+import logging
+
+import numpy as np
+import pandas as pd
+
+from vistautils.parameters import Parameters
+from vistautils.parameters_only_entrypoint import parameters_only_entry_point
+from vistautils.range import Range
+
+
+_log = logging.getLogger(__name__)
+
+
+def random_slice_entry_point(params: Parameters) -> None:
+    """
+    Parameters:
+        input (existing file):
+            The input file. We infer the format from the suffix (e.g.
+
+        output (creatable file):
+            The output file. We infer the desire format from the suffix.
+
+        random_seed (integer):
+            The random seed to use when slicing.
+
+        fraction (decimal):
+            How much of the data to sample. Must be greater than 0 and at most 1. A value of 0.5 for example means
+            "sample 50% of the available data."
+    """
+    input_ = params.existing_file("input")
+    output = params.creatable_file("output")
+    random_seed = params.integer("random_seed", valid_range=Range.at_least(0))
+    fraction = params.floating_point("fraction", valid_range=Range.open_closed(0., 1.))
+    np.random.seed(random_seed)
+
+    df: pd.DataFrame
+    if input_.suffix == "jsonl":
+        df = pd.read_json(str(input_), lines=True)
+    elif input_.suffix in {"csv", "lst"}:
+        df = pd.read_csv(str(input_))
+    else:
+        _log.warning("Don't know how to handle input with suffix %s; attempting to load as CSV...", input_.suffix)
+        df = pd.read_csv(str(input_))
+
+    n_rows = len(df)
+    _log.info("Got input of size %d", n_rows)
+
+    permuted_indices = np.random.permutation(df.index)
+    sampled_indices = permuted_indices[:int(n_rows * fraction)]
+    sampled_df = df[sampled_indices]
+    _log.info("Producing slice of size %d", len(sampled_df))
+
+    if output.suffix == "jsonl":
+        sampled_df.to_json(str(output), lines=True, index=False)
+    elif output.suffix in {"lst"}:
+        sampled_df.to_csv(str(output), header=False, index=False)
+    else:
+        _log.warning("Don't know how to save output with suffix %s; saving as CSV with header...", input_.suffix)
+        sampled_df.to_csv(str(output), header=True, index=False)
+
+
+if __name__ == '__main__':
+    parameters_only_entry_point(random_slice_entry_point)
