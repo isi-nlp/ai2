3ca6bdf856680489abc852a1fb53eff1129807a6
Joe Cecil <jcecil@isi.edu>
2021-05-13 13:14:49 -0400

    Fix debugging

--- ai2/stat_analysis.py
+++ ai2/stat_analysis.py
@@ -66,7 +66,12 @@ def stat_analysis_entrypoint(params: Parameters):
         model1_correct = (model1_predicted_labels == gold_labels)
         model2_correct = (model2_predicted_labels == gold_labels)
         contingency_table = pd.crosstab(model1_correct, model2_correct)
-        logging.debug("contingency table = %s", contingency_table)
+        _logger.info("first entries of model1_predicted_labels = %s", model1_predicted_labels.head())
+        _logger.info("first entries of model2_predicted_labels = %s", model2_predicted_labels.head())
+        _logger.info("-----")
+        _logger.info("first entries of model1_correct = %s", model1_correct.head())
+        _logger.info("first entries of model2_correct = %s", model2_correct.head())
+        _logger.info("contingency table = %s", contingency_table)
         agreement_seq: pd.Series = model1_correct == model2_correct
         agreement_seqs[f"{model1_name} with {model2_name} ({task_name})"] = agreement_seq
         percent_agreement = agreement_seq.mean()
