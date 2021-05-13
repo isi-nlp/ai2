e03eab8c8a311b8f3f081d6f52d1949959ba2baf
Joe Cecil <jcecil@isi.edu>
2021-05-13 13:37:11 -0400

    Log values on own line

--- ai2/stat_analysis.py
+++ ai2/stat_analysis.py
@@ -77,12 +77,12 @@ def stat_analysis_entrypoint(params: Parameters):
         model1_correct = (model1_predicted_labels == gold_labels)
         model2_correct = (model2_predicted_labels == gold_labels)
         contingency_table = pd.crosstab(model1_correct.rename("model1"), model2_correct.rename("model2"))
-        _logger.info("first entries of model1_predicted_labels = %s", model1_predicted_labels.head())
-        _logger.info("first entries of model2_predicted_labels = %s", model2_predicted_labels.head())
+        _logger.info("first entries of model1_predicted_labels = \n%s", model1_predicted_labels.head())
+        _logger.info("first entries of model2_predicted_labels = \n%s", model2_predicted_labels.head())
         _logger.info("-----")
-        _logger.info("first entries of model1_correct = %s", model1_correct.head())
-        _logger.info("first entries of model2_correct = %s", model2_correct.head())
-        _logger.info("contingency table = %s", contingency_table)
+        _logger.info("first entries of model1_correct = \n%s", model1_correct.head())
+        _logger.info("first entries of model2_correct = \n%s", model2_correct.head())
+        _logger.info("contingency table = \n%s", contingency_table)
         agreement_seq: pd.Series = model1_correct == model2_correct
         agreement_seqs[f"{model1_name} with {model2_name} ({task_name})"] = agreement_seq
         percent_agreement = agreement_seq.mean()
