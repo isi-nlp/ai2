89249402a6db40a3ed355dc15153da83573033b2
Joe Cecil <jcecil@isi.edu>
2021-08-24 13:49:33 -0400

    Fix typo: Exact McNemar result should be calculated *exactly*

--- ai2/stat_analysis.py
+++ ai2/stat_analysis.py
@@ -158,7 +158,7 @@ def stat_analysis_entrypoint(params: Parameters):
         # Calculate tests
         test_set_size = len(gold_labels)
         mcnemar_result = library_mcnemar(contingency_table, exact=False, correction=False)
-        exact_mcnemar_result = library_mcnemar(contingency_table, exact=False, correction=False)
+        exact_mcnemar_result = library_mcnemar(contingency_table, exact=True, correction=False)
         stats = {
             "mcnemar": (mcnemar_result.statistic, mcnemar_result.pvalue),
             "mcnemar-exact": (exact_mcnemar_result.statistic, exact_mcnemar_result.pvalue),
